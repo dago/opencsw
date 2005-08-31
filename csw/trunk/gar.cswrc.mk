992f34731c5ae95bb278c81376cfe994f8582d12
comand <comand@d3b55034-1cff-0310-a425-aefe953e1e90>
2005-08-31 23:13:34 +0000

    Allow bits of SPKG_VENDOR to be set independently

--- csw/trunk/gar.cswrc.mk
+++ csw/trunk/gar.cswrc.mk
@@ -1,10 +1,12 @@
 # Packaging information
-SPKG_VENDOR   = $(firstword $(MASTER_SITES)) packaged for CSW by Cory Omand
-SPKG_EMAIL    = comand@blastwave.org
+SPKG_PACKAGER  ?= Cory Omand
+SPKG_SOURCEURL ?= $(firstword $(MASTER_SITES))
+SPKG_VENDOR     = $(SPKG_SOURCEURL) packaged for CSW by $(SPKG_PACKAGER)
+SPKG_EMAIL      = comand@blastwave.org
 # Where to put finished packages
-SPKG_EXPORT   = /export/medusa/comand/staging/build-$(shell date '+%d.%b.%Y')
+SPKG_EXPORT     = /export/medusa/comand/staging/build-$(shell date '+%d.%b.%Y')
 # Where to put downloaded sources (make garchive)
-GARCHIVEDIR   = /export/medusa/comand/src
+GARCHIVEDIR     = /export/medusa/comand/src
 # Where to look for DISTFILES before downloading
-GARCHIVEPATH  = /export/medusa/comand/src
-GARCHIVEPATH += /export/medusa/src
+GARCHIVEPATH    = /export/medusa/comand/src
+GARCHIVEPATH   += /export/medusa/src
