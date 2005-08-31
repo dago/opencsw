926b2c3b14ba33d080bc3bc4c047e190e062e69d
comand <comand@d3b55034-1cff-0310-a425-aefe953e1e90>
2005-08-31 23:01:03 +0000

    Packaging files for PHP5

---
+++ csw/trunk/lang/php5/files/extensions.mk
@@ -0,0 +1,23 @@
+
+# PHP Extensions
+
+DISTFILES += CSWphp5bz2.gspec CSWphp5bz2.prototype CSWphp5bz2.depend
+DISTFILES += CSWphp5curl.gspec CSWphp5curl.prototype CSWphp5curl.depend
+DISTFILES += CSWphp5dba.gspec CSWphp5dba.prototype CSWphp5dba.depend
+DISTFILES += CSWphp5devel.gspec CSWphp5devel.prototype CSWphp5devel.depend
+DISTFILES += CSWphp5fam.gspec CSWphp5fam.prototype CSWphp5fam.depend
+DISTFILES += CSWphp5gd.gspec CSWphp5gd.prototype CSWphp5gd.depend
+DISTFILES += CSWphp5gettext.gspec CSWphp5gettext.prototype CSWphp5gettext.depend
+DISTFILES += CSWphp5gmp.gspec CSWphp5gmp.prototype CSWphp5gmp.depend
+DISTFILES += CSWphp5ldap.gspec CSWphp5ldap.prototype CSWphp5ldap.depend
+DISTFILES += CSWphp5mssql.gspec CSWphp5mssql.prototype CSWphp5mssql.depend
+DISTFILES += CSWphp5mysql.gspec CSWphp5mysql.prototype CSWphp5mysql.depend
+DISTFILES += CSWphp5mysqli.gspec CSWphp5mysqli.prototype CSWphp5mysqli.depend
+DISTFILES += CSWphp5odbc.gspec CSWphp5odbc.prototype CSWphp5odbc.depend
+DISTFILES += CSWphp5pgsql.gspec CSWphp5pgsql.prototype CSWphp5pgsql.depend
+DISTFILES += CSWphp5pspell.gspec CSWphp5pspell.prototype CSWphp5pspell.depend
+DISTFILES += CSWphp5readline.gspec CSWphp5readline.prototype CSWphp5readline.depend
+DISTFILES += CSWphp5snmp.gspec CSWphp5snmp.prototype CSWphp5snmp.depend
+DISTFILES += CSWphp5sqlite.gspec CSWphp5sqlite.prototype CSWphp5sqlite.depend
+DISTFILES += CSWphp5xsl.gspec CSWphp5xsl.prototype CSWphp5xsl.depend
+
