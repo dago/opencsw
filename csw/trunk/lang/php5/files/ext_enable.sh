a59baba0dc03fd4b25c8336a69a1070165d3d1b1
comand <comand@d3b55034-1cff-0310-a425-aefe953e1e90>
2005-09-01 02:05:12 +0000

    Generic scripts for enabling/disabling loadable extensions in php.ini.

---
+++ csw/trunk/lang/php5/files/ext_enable.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+CSW_PREFIX=${PKG_INSTALL_ROOT}/opt/csw
+PHP_INI=$CSW_PREFIX/php5/lib/php.ini
+
+PKG_INSTALL_ROOT=${PKG_INSTALL_ROOT:-'/'}
+
+PHPEXT=`echo $PKGINST | sed -e 's,CSWphp5,,' -e 's,\..*,,'`
+PHPEXT=${PHPEXT:-${PKGINST}}
+
+# Bail if php.ini does not exist
+if [ ! -f $PHP_INI ]; then
+    cat << END
+Cannot locate $PHP_INI -- Please enable $PHPEXT.so manually
+in the appropriate php.ini file.
+
+END
+
+    exit 0
+fi
+
+/usr/bin/perl -i.bak -plne "s,;(extension=$PHPEXT.so),\$1," $PHP_INI
+
+echo "PHP extension $PHPEXT.so is enabled in $PHP_INI."
+
+exit 0
+
