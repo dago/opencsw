cd2e99e8df84879dff9bf4f73b96bcf8e24d4751
comand <comand@d3b55034-1cff-0310-a425-aefe953e1e90>
2005-10-31 18:30:08 +0000

    Added a patch to force apu to use GNU libiconv.

--- csw/trunk/server/apache2/files/rules.mk
+++ csw/trunk/server/apache2/files/rules.mk
@@ -1,4 +1,10 @@
-pre-configure:
+pre-configure: install-config-layout apu-make-configure
+
+install-config-layout:
 	@sed -e s,INSTALL_PREFIX,$(prefix)/apache2,g \
 		$(WORKDIR)/config.layout > $(WORKSRC)/config.layout
+	@$(MAKECOOKIE)
+
+apu-make-configure:
+	@( cd $(WORKSRC)/srclib/apr-util ; autoconf )
 	@$(MAKECOOKIE)
