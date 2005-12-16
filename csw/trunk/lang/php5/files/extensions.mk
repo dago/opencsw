d5c94473c8f26b0ad45d1fec9a09b6edf3c350f8
comand <comand@d3b55034-1cff-0310-a425-aefe953e1e90>
2005-12-16 07:23:16 +0000

    Major update to 5.1.1 + Apache 2.2

--- csw/trunk/lang/php5/files/extensions.mk
+++ csw/trunk/lang/php5/files/extensions.mk
@@ -9,13 +9,17 @@ XLIST += gmp
 XLIST += ldap
 XLIST += mssql
 XLIST += mysql
+XLIST += pdomysql
 XLIST += mysqli
 XLIST += odbc
+XLIST += pdoodbc
 XLIST += pgsql
+XLIST += pdopgsql
 XLIST += pspell
 XLIST += readline
 XLIST += snmp
 XLIST += sqlite
+XLIST += pdosqlite
 XLIST += xsl
 
 DISTFILES += $(foreach N,$(XLIST),$(call admfiles,CSWphp5$(N),prototype depend))
