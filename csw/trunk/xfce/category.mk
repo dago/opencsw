e23b3f2e4dcb2c6d9d7eed6454602ebd2b2dddfe
wbonnet <wbonnet@d3b55034-1cff-0310-a425-aefe953e1e90>
2007-01-31 22:46:47 +0000

    Adding Xfce 4.4.0

--- csw/trunk/xfce/category.mk
+++ csw/trunk/xfce/category.mk
@@ -1,1 +1,20 @@
+# XFCE
+XFCE_ROOT      = http://www.us.xfce.org
+XFCE_VERSION   = 4.4.0
+XFCE_MIRROR    = $(XFCE_ROOT)/archive/xfce-$(XFCE_VERSION)/src/
+
+MASTER_SITES ?= $(XFCE_MIRROR)
+GARVERSION   ?= $(XFCE_VERSION)
+PKGDIST      ?= $(GARNAME)-$(GARVERSION).tar.bz2
+DISTFILES    += $(PKGDIST)
+
 include ../../gar.mk
+
+# Compiler options
+CFLAGS   += -xlibmil
+CXXFLAGS += -xlibmil -xlibmopt -features=tmplife -norunpath
+CPPFLAGS += -I$(DESTDIR)/opt/csw/include/xfce4
+LDFLAGS  += -L/opt/csw/lib -R/opt/csw/lib
+
+# Configure common options
+CONFIGURE_ARGS = --prefix=/opt/csw --disable-debug --enable-final --mandir=/opt/csw/share/man
