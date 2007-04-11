5054c56b5700e07d60434ab79a5858272e9833cd
wbonnet <wbonnet@d3b55034-1cff-0310-a425-aefe953e1e90>
2007-04-11 21:29:48 +0000

    Switch to version 4.4.1

--- csw/trunk/xfce/category.mk
+++ csw/trunk/xfce/category.mk
@@ -1,6 +1,6 @@
 # XFCE
 XFCE_ROOT      = http://www.us.xfce.org
-XFCE_VERSION   = 4.4.0
+XFCE_VERSION   = 4.4.1
 XFCE_MIRROR    = $(XFCE_ROOT)/archive/xfce-$(XFCE_VERSION)/src/
 
 MASTER_SITES ?= $(XFCE_MIRROR)
@@ -11,12 +11,21 @@ DISTFILES    += $(PKGDIST)
 include ../../gar.mk
 
 # Compiler options
-CFLAGS   += -xlibmil
+# Compiler options
+CFLAGS   += -xlibmil -errtags=yes -erroff=E_EMPTY_DECLARATION
 CXXFLAGS += -xlibmil -xlibmopt -features=tmplife -norunpath
-EXTRA_INC = $(DESTDIR)$(includedir)/xfce4
+CPPFLAGS += -I$(DESTDIR)/opt/csw/include/xfce4
+LDFLAGS  += -L/opt/csw/lib -R/opt/csw/lib
+
+# pkg-config options
+PKG_CONFIG_PATH += $(DESTDIR)/opt/csw/lib/pkgconfig
 
 # Configure common options
 CONFIGURE_ARGS  = $(DIRPATHS)
+# CONFIGURE_ARGS += --prefix=/opt/csw
 CONFIGURE_ARGS += --disable-debug
 CONFIGURE_ARGS += --enable-final
+CONFIGURE_ARGS += --enable-xinerama
+CONFIGURE_ARGS += --enable-dbus
+# CONFIGURE_ARGS += --mandir=/opt/csw/share/man
 
