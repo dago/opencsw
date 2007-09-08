bd4abcae3ce5c6bb1200ccc2342650e0fe1d0e9a
dam <dam@d3b55034-1cff-0310-a425-aefe953e1e90>
2007-09-08 14:08:47 +0000

    qt4: Adjustements in prototypes

--- csw/branches/dam/package-install-separation/lib/qt4/files/split-proto.pl
+++ csw/branches/dam/package-install-separation/lib/qt4/files/split-proto.pl
@@ -1,5 +1,12 @@
 #!/usr/bin/perl
 
+sub footer {
+  my $pkg = shift;
+  return "i copyright=${pkg}.copyright\n" .
+	"i depend=${pkg}.depend\n" .
+	"i pkginfo=${pkg}.pkginfo\n";
+}
+
 open MAIN_SPARC, ">CSWqt4.prototype-sparc";
 open MAIN_I386, ">CSWqt4.prototype-i386";
 open DEVEL_SPARC, ">CSWqt4-devel.prototype-sparc";
@@ -11,7 +18,9 @@ open P, "prototype";
 while( <P> ) {
   $_ =~ s/\? \? \?/0755 root bin/;
   my ($type, $class, $pathname, $mode, $owner, $group, $rest) = split( /\s+/, $_, 7 );
-  if( $pathname =~ m!^/opt/csw/share/doc! ) {
+  if( $pathname =~ m!^/opt/csw/ignore! ) {
+    # Ignore this line
+  } elsif( $pathname =~ m!^/opt/csw/share/doc! ) {
     print DOC $_;
   } elsif( $pathname =~ m!^/opt/csw/include! ||
 	$pathname =~ m!\.(la|prl)$! ||
@@ -20,7 +29,10 @@ while( <P> ) {
     print DEVEL_SPARC $_;
     $_ =~ s!/sparcv9!/amd64!;
     print DEVEL_I386 $_;
-  } elsif( $pathname =~ m!^/opt/csw/(ignore|share/man|share/perl|lib/perl)! ) {
+  } elsif( $pathname =~ m!^/opt/csw/(share/man|share/perl|lib/perl)! ) {
+    # Ignore this line
+  } elsif( $class =~ /^(copyright|depend|pkginfo)=/ ) {
+    # Ignore this line
   } else {
     print MAIN_SPARC $_;
     $_ =~ s!/sparcv9!/amd64!;
@@ -28,3 +40,9 @@ while( <P> ) {
   }
 }
 close P;
+
+print MAIN_SPARC footer( "CSWqt4" );
+print MAIN_I386 footer( "CSWqt4" );
+print DEVEL_SPARC footer( "CSWqt4-devel" );
+print DEVEL_I386 footer( "CSWqt4-devel" );
+print DOC footer( "CSWqt4-docs" );
