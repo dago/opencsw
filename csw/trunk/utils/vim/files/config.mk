80cd29f83174eed63d0eeecf88a198f617b420cb
comand <comand@d3b55034-1cff-0310-a425-aefe953e1e90>
2007-11-19 01:46:41 +0000

    Adding a common configuration makefile used by both vim and gvim.

--- csw/trunk/utils/vim/files/config.mk
+++ csw/trunk/utils/vim/files/config.mk
@@ -10,10 +10,10 @@ CONFIGURE_ARGS += $(DIRPATHS)
 CONFIGURE_ARGS += --with-global-runtime=$(sharedstatedir)/$(GARNAME)
 CONFIGURE_ARGS += --with-features=huge
 CONFIGURE_ARGS += --with-tlib=ncurses
-CONFIGURE_ARGS += --enable-perlinterp
-CONFIGURE_ARGS += --enable-pythoninterp
-CONFIGURE_ARGS += --with-python-config-dir=$(libdir)/python/config/
-CONFIGURE_ARGS += --enable-tclinterp
+#CONFIGURE_ARGS += --enable-perlinterp
+#CONFIGURE_ARGS += --enable-pythoninterp
+#CONFIGURE_ARGS += --with-python-config-dir=$(libdir)/python/config/
+#CONFIGURE_ARGS += --enable-tclinterp
 #CONFIGURE_ARGS += --enable-rubyinterp
 CONFIGURE_ARGS += --enable-multibyte
 CONFIGURE_ARGS += --enable-cscope
