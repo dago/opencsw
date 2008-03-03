829809f6759c1e9539b5008b91696bc0275e46de
dam <dam@d3b55034-1cff-0310-a425-aefe953e1e90>
2008-03-03 13:58:44 +0000

    modularpkg: Tree reorg, partial commit

--- csw/branches/modularpkg/gar/category.mk
+++ csw/branches/modularpkg/gar/category.mk
@@ -1,6 +1,13 @@
 # vim: ft=make ts=4 sw=4 noet
 # This makefile is to be included from Makefiles in each category
 # directory.
+
+ifeq (,$(CATEGORIES))
+  $(error Please set CATEGORIES to one of $(wildcard categories/*))
+fi
+
+include categories/$(CATEGORIES)/category.mk
+
 %:
 	@for i in $(filter-out CVS/,$(wildcard */)) ; do \
 		$(MAKE) -C $$i $* ; \
