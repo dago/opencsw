04f9b021c6d63db6c65f70342bd71561a9e63715
wbonnet <wbonnet@d3b55034-1cff-0310-a425-aefe953e1e90>
2008-12-16 22:15:30 +0000

    Update default regexp for upstream detection

--- csw/mgar/gar/v2/categories/cpan/category.mk
+++ csw/mgar/gar/v2/categories/cpan/category.mk
@@ -28,8 +28,8 @@ SPKG_SOURCEURL  = http://search.cpan.org
 SPKG_SOURCEURL := $(SPKG_SOURCEURL)/~$(call TOLOWER,$(AUTHOR))
 
 # We define upstream file regex so we can be notifed of new upstream software release
-UFILES_REGEX = $(GARNAME)-(\d+(?:\.\d+)*).tar.gz
-USTREAM_MASTER_SITE = $(SPKG_SOURCEURL)
+UFILES_REGEX ?= $(GARNAME)-(\d+(?:\.\d+)*).tar.gz
+USTREAM_MASTER_SITE ?= $(SPKG_SOURCEURL)
 
 SPKG_SOURCEURL := $(SPKG_SOURCEURL)/$(GARNAME)
 
