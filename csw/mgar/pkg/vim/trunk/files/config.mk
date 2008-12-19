90da8ed257e7884ce3b4a48c6a5eaf9aa536a421
harpchad <harpchad@d3b55034-1cff-0310-a425-aefe953e1e90>
2008-12-19 18:51:27 +0000

    Updated for vim72, patches available from vim.org are no longer maintained in gar svn

--- csw/mgar/pkg/vim/trunk/files/config.mk
+++ csw/mgar/pkg/vim/trunk/files/config.mk
@@ -1,4 +1,7 @@
-MASTER_SITES = ftp://ftp.vim.org/pub/vim/unix/
+MASTER_SITES += ftp://ftp.vim.org/pub/vim/patches/$(DISTVERSION)/
+MASTER_SITES += ftp://ftp.vim.org/pub/vim/extra/
+MASTER_SITES += ftp://ftp.vim.org/pub/vim/unix/
+
 DISTFILES += $(GARNAME)-$(DISTVERSION).tar.bz2
 DISTFILES += $(GARNAME)-$(DISTVERSION)-lang.tar.gz
 DISTFILES += $(GARNAME)-$(DISTVERSION)-extra.tar.gz
@@ -10,13 +13,7 @@ CONFIGURE_ARGS += $(DIRPATHS)
 CONFIGURE_ARGS += --with-global-runtime=$(sharedstatedir)/$(GARNAME)
 CONFIGURE_ARGS += --with-features=huge
 CONFIGURE_ARGS += --with-tlib=ncurses
-#CONFIGURE_ARGS += --enable-perlinterp
-#CONFIGURE_ARGS += --enable-pythoninterp
-#CONFIGURE_ARGS += --with-python-config-dir=$(libdir)/python/config/
-#CONFIGURE_ARGS += --enable-tclinterp
-#CONFIGURE_ARGS += --enable-rubyinterp
 CONFIGURE_ARGS += --enable-multibyte
 CONFIGURE_ARGS += --enable-cscope
-CONFIGURE_ARGS += --disable-static
-CONFIGURE_ARGS += --enable-shared
-
+BUILD_ARGS += "VIMRCLOC=/opt/csw/etc/vim"
+BUILD_ARGS += "VIMRUNTIMEDIR=/opt/csw/share/vim/vim72"
--- csw/mgar/pkg/vim/trunk/files/patches.mk
+++ csw/mgar/pkg/vim/trunk/files/patches.mk
@@ -1,27 +1,8 @@
-# Vim 71 patchset
-PATCHREV = 147
-PATCHDIRLEVEL = 2
-#PATCHFILES += 7.1.001-$(PATCHREV)
+# Vim 72 patchset
+PATCHREV = 069
+PATCHDIRLEVEL = 0
 GARVERSION = $(DISTVERSION).$(PATCHREV)
-
-DISTPATCHES += $(foreach T,$(shell gseq -w 001 145),$(DISTVERSION).$(T))
-NOPATCH  = 7.1.015 7.1.021 7.1.040 7.1.042 7.1.052 7.1.063 7.1.071 7.1.120
-NOPATCH += 7.1.114
-DISTPATCHES := $(filter-out $(NOPATCH),$(DISTPATCHES))
-SUPERPATCH = $(DISTVERSION).001-$(PATCHREV)
-
-# Vim distributed patches, minus ones that won't apply cleanly.
-PATCHFILES += $(SUPERPATCH)
+PATCHFILES += $(foreach T,$(shell gseq -f "%03g" 001 $(PATCHREV)),$(DISTVERSION).$(T))
 
 # Fix vimtutor
 PATCHFILES += vimtutor.diff
-
-#DISTPATCHFILES = $(addprefix files/,$(DISTPATCHES))
-#superpatch:
-#	@echo " ==> Creating $(SUPERPATCH)"
-#	@echo " ==> Skipping patches: $(NOPATCH)"
-#	@-rm -f files/$(SUPERPATCH)
-#	@for p in $(DISTPATCHFILES) ; do \
-#	    cat $$p >> files/$(SUPERPATCH) ; \
-#	done
-
