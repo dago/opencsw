b7dff03fe7c83b1e45fb689d64bbd0a2c6391815
harpchad <harpchad@d3b55034-1cff-0310-a425-aefe953e1e90>
2009-01-26 19:33:45 +0000

    - update to patch level 088 - now using gar v2 - now includes 64 bit binaries (w/isaexec)

--- csw/mgar/pkg/vim/trunk/files/patches.mk
+++
@@ -1,8 +0,0 @@
-# Vim 72 patchset
-PATCHREV = 082
-PATCHDIRLEVEL = 0
-GARVERSION = $(DISTVERSION).$(PATCHREV)
-PATCHFILES += $(foreach T,$(shell gseq -f "%03g" 001 $(PATCHREV)),$(DISTVERSION).$(T))
-
-# Fix vimtutor
-PATCHFILES += vimtutor.diff
