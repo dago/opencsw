08a21f52d51ba6dbbea1fc7578b468d90236619c
valholla <valholla@d3b55034-1cff-0310-a425-aefe953e1e90>
2009-02-20 22:08:27 +0000

    Initial Checkin

---
+++ csw/mgar/pkg/fixme/fixme.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+####################################################
+#
+#  fixme.sh
+#  Intended to remove the use of libtool's 
+#  pseudo-libraries for opencsw builds.
+#
+#  Author: Mike Watters  mwatters_at_opencsw.org
+#  Initial Version: 0.1
+#
+####################################################
+
+umask 0022
+PATH=/opt/csw/bin
+
+if [ $# -ne 1 ]; then
+    gecho "USAGE: $(basename $0) WORKSRC"
+    exit 1
+fi
+BASEPATH=$1
+
+## Fix Makefiles
+for mk in $(gfind ${BASEPATH} -name Makefile -print); do
+    LT_FILES=$(ggrep '/opt/csw.*/lib/.*.la' ${mk} | \
+        gsed "s/^.*\(\/opt\/csw.*\/lib\/.*\.la\).*$/\1/")
+    
+    for file in ${LT_FILES}; do
+        LIB_NAME=$(ggrep 'dlname=' ${file} | \
+            gsed -e "s/.*'\(.*\)'/\1/" \
+                -e "s/^lib//" \
+                -e "s/\.so.*$//")
+        fixpath=$(gecho $file |gsed 's/\//\\\//g')
+        sed "s/${fixpath}/-l${LIB_NAME}/g" ${mk} >Makefile.new
+        mv Makefile.new ${mk}
+    done
+done
+
+## Fix libtool Script
+for lt in $(gfind ${BASEPATH} -name libtool -print); do
+    gsed "/for search_ext in .*\.la/s/\.la//" ${lt} >${lt}.new
+    mv ${lt}.new ${lt}
+done
+
