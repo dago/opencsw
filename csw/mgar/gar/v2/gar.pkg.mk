5886efb90d1e9ebab33e28bc49d91a263cd6d84d
dmichelsen <dmichelsen@d3b55034-1cff-0310-a425-aefe953e1e90>
2009-02-20 15:21:06 +0000

    mGAR v2: Fix pathfilter on packages without any includes or excludes

--- csw/mgar/gar/v2/gar.pkg.mk
+++ csw/mgar/gar/v2/gar.pkg.mk
@@ -241,7 +241,7 @@ $(WORKDIR)/%.prototype: | $(PROTOTYPE)
 	      -n "$(_PKGFILES_EXCLUDE)" -o \
 	      -n "$(ISAEXEC_FILES_$*)" -o \
 	      -n "$(ISAEXEC_FILES)" ]; then \
-	  (pathfilter -i $(call licensedir,$*)/license \
+	  (pathfilter $(if $(or $(_PKGFILES_EXCLUDE),$(_PKGFILES_INCLUDE)),-i $(call licensedir,$*)/license) \
 		      $(foreach S,$(filter-out $*,$(SPKG_SPECS)),-x $(call licensedir,$S)/license) \
 		      $(foreach FILE,$(_PKGFILES_INCLUDE),-i '$(FILE)') \
 		      $(if $(_PKGFILES_INCLUDE),-x '.*',$(foreach FILE,$(_PKGFILES_EXCLUDE),-x '$(FILE)')) \
