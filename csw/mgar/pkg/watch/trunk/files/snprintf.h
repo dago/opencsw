4b9ad99b3a3d20dc71374937f1a6224176603586
skayser <skayser@d3b55034-1cff-0310-a425-aefe953e1e90>
2009-03-04 22:52:35 +0000

    watch: fixed asprintf()

--- csw/mgar/pkg/watch/trunk/files/snprintf.h
+++ csw/mgar/pkg/watch/trunk/files/snprintf.h
@@ -1,26 +1,29 @@
-#ifndef _PORTABLE_SNPRINTF_H_
-#define _PORTABLE_SNPRINTF_H_
+/* $Id: system.h,v 1.1.1.1 2008/01/06 03:24:00 holger Exp $ */
 
-#define PORTABLE_SNPRINTF_VERSION_MAJOR 2
-#define PORTABLE_SNPRINTF_VERSION_MINOR 2
+#ifndef SYSTEM_H
+#define SYSTEM_H
 
-#ifdef HAVE_SNPRINTF
-#include <stdio.h>
-#else
-extern int snprintf(char *, size_t, const char *, /*args*/ ...);
-extern int vsnprintf(char *, size_t, const char *, va_list);
-#endif
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif	/* HAVE_CONFIG_H */
 
-#if defined(HAVE_SNPRINTF) && defined(PREFER_PORTABLE_SNPRINTF)
-extern int portable_snprintf(char *str, size_t str_m, const char *fmt, /*args*/ ...);
-extern int portable_vsnprintf(char *str, size_t str_m, const char *fmt, va_list ap);
-#define snprintf  portable_snprintf
-#define vsnprintf portable_vsnprintf
-#endif
-
-extern int asprintf  (char **ptr, const char *fmt, /*args*/ ...);
-extern int vasprintf (char **ptr, const char *fmt, va_list ap);
-extern int asnprintf (char **ptr, size_t str_m, const char *fmt, /*args*/ ...);
-extern int vasnprintf(char **ptr, size_t str_m, const char *fmt, va_list ap);
-
-#endif
+#if HAVE_STDARG_H
+#include <stdarg.h>
+#if !HAVE_VSNPRINTF
+int rpl_vsnprintf(char *, size_t, const char *, va_list);
+#define vsnprintf rpl_vsnprintf
+#endif	/* !HAVE_VSNPRINTF */
+#if !HAVE_SNPRINTF
+int rpl_snprintf(char *, size_t, const char *, ...);
+#define snprintf rpl_snprintf
+#endif	/* !HAVE_SNPRINTF */
+#if !HAVE_VASPRINTF
+int rpl_vasprintf(char **, const char *, va_list);
+#define vasprintf rpl_vasprintf
+#endif	/* !HAVE_VASPRINTF */
+#if !HAVE_ASPRINTF
+int rpl_asprintf(char **, const char *, ...);
+#define asprintf rpl_asprintf
+#endif	/* !HAVE_ASPRINTF */
+#endif	/* HAVE_STDARG_H */
+#endif	/* SYSTEM_H */
