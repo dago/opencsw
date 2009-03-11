90b6effa0c401aec84817d9dc3513aec03b2d681
valholla <valholla@d3b55034-1cff-0310-a425-aefe953e1e90>
2009-03-11 17:41:57 +0000

    working copy

--- csw/mgar/pkg/subversion/trunk/files/fixme.sh
+++ csw/mgar/pkg/subversion/trunk/files/fixme.sh
@@ -1,36 +1,49 @@
 #!/bin/bash
-
-touch /tmp/mike-fixme
+####################################################
+#
+#  fixme.sh
+#  Intended to remove the use of libtool's 
+#  pseudo-libraries for opencsw builds.
+#
+#  Author: Mike Watters  mwatters_at_opencsw.org
+#  Initial Version: 0.1
+#
+####################################################
 
 umask 0022
-PATH=/opt/csw/bin:/usr/bin:/sbin
+PATH=/opt/csw/bin
 
 if [ $# -ne 1 ]; then
-    echo "USAGE: $(basename $0) WORKSRC"
+    gecho "USAGE: $(basename $0) WORKSRC"
     exit 1
 fi
 BASEPATH=$1
 
 ## Fix Makefiles
 for mk in $(gfind ${BASEPATH} -name Makefile -print); do
-    LT_FILES=$(ggrep '/opt/csw.*/lib/.*.la' ${mk} | \
+    LT_FILES=$(ggrep '/opt/csw.*/lib/.*\.la' ${mk} | \
         gsed "s/^.*\(\/opt\/csw.*\/lib\/.*\.la\).*$/\1/")
     
     for file in ${LT_FILES}; do
-        LIB_NAME=$(ggrep dlname= ${file} | \
+        LIB_NAME=$(ggrep 'dlname=' ${file} | \
             gsed -e "s/.*'\(.*\)'/\1/" \
                 -e "s/^lib//" \
                 -e "s/\.so.*$//")
-        fixpath=$(echo $file |gsed 's/\//\\\//g')
-        sed "s/${fixpath}/-l${LIB_NAME}/g" \
-            ${mk} >Makefile.new
-        mv Makefile.new ${mk}
+        fixpath=$(gecho $file |gsed 's/\//\\\//g')
+        gsed "s/${fixpath}/-l${LIB_NAME}/g" ${mk} >Makefile.new
+        gmv Makefile.new ${mk}
     done
 done
 
-## Fix libtool
+## Fix libtool Script
 for lt in $(gfind ${BASEPATH} -name libtool -print); do
     gsed "/for search_ext in .*\.la/s/\.la//" ${lt} >${lt}.new
-    mv ${lt}.new ${lt}
+    gmv ${lt}.new ${lt}
 done
+
+LTMAIN=$(gfind ${BASEPATH} -name ltmain.sh -print)
+if [ -f ${LTMAIN} ]; then
+    gsed "/for search_ext in .*\.la/s/\.la//" ${LTMAIN} >${LTMAIN}.new
+    gmv ${LTMAIN}.new ${LTMAIN}
+fi
 
