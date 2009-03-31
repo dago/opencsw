4dd5eeda15e1dafe39acf20d68c2281fddc69068
valholla <valholla@d3b55034-1cff-0310-a425-aefe953e1e90>
2009-03-31 15:22:09 +0000

    working commit

--- csw/mgar/pkg/gcc4/trunk/files/amd_merge.mk
+++ csw/mgar/pkg/gcc4/trunk/files/amd_merge.mk
@@ -1,4 +1,12 @@
 
+AMD_DEBUG = 1
+
+ifeq ($(AMD_DEBUG),)
+	_DBG=@
+else
+	_DBG=
+endif   
+
 AMD_BASE  = $(WORKROOTDIR)/install-isa-i386-5.10-i386/
 I386_BASE = $(WORKROOTDIR)/install-isa-i386-5.8-i386/
 MPREFIX   = opt/csw/gcc4
@@ -6,44 +14,45 @@ APREFIX   = $(AMD_BASE)/$(MPREFIX)
 IPREFIX   = $(I386_BASE)/$(MPREFIX)
 PPREFIX   = $(PKGROOT)/$(MPREFIX)
 
-AMD_MERGE_TARGETS  = x86-merge-dirs
-AMD_MERGE_TARGETS += x86-merge-i386
-AMD_MERGE_TARGETS += x86-merge-amd
-AMD_MERGE_TARGETS += x86-merge-strip
+AMD_MERGE_TARGETS  = merge-dirs-amd
+AMD_MERGE_TARGETS += merge-i386-files
+AMD_MERGE_TARGETS += merge-amd64-files
+AMD_MERGE_TARGETS += merge-strip-amd
 
 merge-amd: $(AMD_MERGE_TARGETS)
-	@$(MAKECOOKIE)
+	$(_DBG)$(MAKECOOKIE)
 
-x86-merge-dirs:
-	@(ginstall -d $(PKGROOT))
-	@(ginstall -d $(PPREFIX)/bin/amd64)
-	@(ginstall -d $(PPREFIX)/bin/i386)
-	@$(MAKECOOKIE)
+merge-dirs-amd:
+	$(_DBG)(ginstall -d $(PKGROOT))
+	$(_DBG)(ginstall -d $(PPREFIX)/bin/amd64)
+	$(_DBG)(ginstall -d $(PPREFIX)/bin/i386)
+	$(_DBG)$(MAKECOOKIE)
 
-x86-merge-amd:
-	@(echo "[===== Merging ISA-AMD64 =====]")
-	@(cd $(AMD_BASE); for dir in `gfind . -name "*solaris2\.10*" -type d` ; do \
+merge-amd64-files:
+	$(call _pmod,Merging isa-amd64)
+	@(echo "[===== Merging isa-amd64: ISA=$(ISA) =====]")
+	$(_DBG)(cd $(AMD_BASE); for dir in `gfind . -name "*solaris2\.10*" -type d` ; do \
 		/usr/bin/pax -rw $$dir $(PKGROOT); done )
-	@(cd $(AMD_BASE); /usr/bin/pax -rw $(MPREFIX)/lib/amd64 $(PKGROOT))
-	@(cd $(APREFIX)/bin; /usr/bin/pax -rw * $(PPREFIX)/bin/amd64)
-	@$(MAKECOOKIE)
+	$(_DBG)(cd $(AMD_BASE); /usr/bin/pax -rw $(MPREFIX)/lib/amd64 $(PKGROOT))
+	$(_DBG)(cd $(APREFIX)/bin; /usr/bin/pax -rw * $(PPREFIX)/bin/amd64)
+	$(_DBG)$(MAKECOOKIE)
 
-x86-merge-i386:
+merge-i386-files:
 	@(echo "[===== Merging ISA-I386 =====]")
-	@(cd $(I386_BASE); /usr/bin/pax -rw $(MPREFIX)/include $(PKGROOT))
-	@(cd $(I386_BASE); /usr/bin/pax -rw $(MPREFIX)/info $(PKGROOT))
-	@(cd $(I386_BASE); /usr/bin/pax -rw $(MPREFIX)/man $(PKGROOT))
-	@(cd $(I386_BASE); /usr/bin/pax -rw $(MPREFIX)/share $(PKGROOT))
-	@(cd $(I386_BASE); /usr/bin/pax -rw $(MPREFIX)/lib $(PKGROOT))
-	@(cd $(I386_BASE); /usr/bin/pax -rw $(MPREFIX)/libexec $(PKGROOT))
-	@(cd $(I386_BASE); /usr/bin/pax -rw $(MPREFIX)/bin $(PKGROOT))
-	@(gmv $(PPREFIX)/bin/i386-pc* $(PPREFIX)/bin/i386/)
-	@$(MAKECOOKIE)
+	$(_DBG)(cd $(I386_BASE); /usr/bin/pax -rw $(MPREFIX)/bin $(PKGROOT))
+	$(_DBG)(gmv -f $(PPREFIX)/bin/i386-pc* $(PPREFIX)/bin/i386/)
+	$(_DBG)(cd $(I386_BASE); /usr/bin/pax -rw $(MPREFIX)/include $(PKGROOT))
+	$(_DBG)(cd $(I386_BASE); /usr/bin/pax -rw $(MPREFIX)/info $(PKGROOT))
+	$(_DBG)(cd $(I386_BASE); /usr/bin/pax -rw $(MPREFIX)/man $(PKGROOT))
+	$(_DBG)(cd $(I386_BASE); /usr/bin/pax -rw $(MPREFIX)/share $(PKGROOT))
+	$(_DBG)(cd $(I386_BASE); /usr/bin/pax -rw $(MPREFIX)/lib $(PKGROOT))
+	$(_DBG)(cd $(I386_BASE); /usr/bin/pax -rw $(MPREFIX)/libexec $(PKGROOT))
+	$(_DBG)$(MAKECOOKIE)
 
-x86-merge-strip:
+merge-strip-amd:
 	@(echo "[===== Stripping Merged Binaries =====]")
-	@(stripbin $(PPREFIX)/bin)
-	@(stripbin $(PPREFIX)/bin/i386)
-	@(stripbin $(PPREFIX)/bin/amd64)
-	@$(MAKECOOKIE)
+	$(_DBG)(stripbin $(PPREFIX)/bin/i386)
+	$(_DBG)(stripbin $(PPREFIX)/bin/amd64)
+	$(_DBG)(stripbin $(PPREFIX)/bin)
+	$(_DBG)$(MAKECOOKIE)
 
