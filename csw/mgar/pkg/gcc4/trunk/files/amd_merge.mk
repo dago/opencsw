026c64c29a8bc511a4982217f33a98f5173bd38b
valholla <valholla@d3b55034-1cff-0310-a425-aefe953e1e90>
2009-04-30 15:54:50 +0000

    fix isaexec

--- csw/mgar/pkg/gcc4/trunk/files/amd_merge.mk
+++ csw/mgar/pkg/gcc4/trunk/files/amd_merge.mk
@@ -1,3 +1,4 @@
+AMD_DEBUG = 1
 ifeq ($(AMD_DEBUG),)
 	_DBG=@
 else
@@ -30,31 +31,30 @@ merge-fix-links:
 	@echo "[===== Merging Fixing Hard Links =====]"
 	$(_DBG)(cd $(IPREFIX)/bin; grm -f *gcc *c++ g++ gcj gfortran)
 	$(_DBG)(cd $(IPREFIX)/bin; \
-		gcp sparc-sun-solaris2.8-gcc-4.3.3 sparc-sun-solaris2.8-gcc)
-	$(_DBG)(cd $(IPREFIX)/bin; gcp sparc-sun-solaris2.8-gcc-4.3.3 gcc)
+		gcp i386-pc-solaris2.8-gcc-4.3.3 i386-pc-solaris2.8-gcc)
+	$(_DBG)(cd $(IPREFIX)/bin; gcp i386-pc-solaris2.8-gcc-4.3.3 gcc)
 	$(_DBG)(cd $(IPREFIX)/bin; \
-		gcp sparc-sun-solaris2.8-g++ sparc-sun-solaris2.8-c++)
-	$(_DBG)(cd $(IPREFIX)/bin; gcp sparc-sun-solaris2.8-g++ g++)
-	$(_DBG)(cd $(IPREFIX)/bin; gcp sparc-sun-solaris2.8-g++ c++)
-	$(_DBG)(cd $(IPREFIX)/bin; gcp sparc-sun-solaris2.8-gcj gcj)
-	$(_DBG)(cd $(IPREFIX)/bin; gcp sparc-sun-solaris2.8-gfortran gfortran)
+		gcp i386-pc-solaris2.8-g++ i386-pc-solaris2.8-c++)
+	$(_DBG)(cd $(IPREFIX)/bin; gcp i386-pc-solaris2.8-g++ g++)
+	$(_DBG)(cd $(IPREFIX)/bin; gcp i386-pc-solaris2.8-g++ c++)
+	$(_DBG)(cd $(IPREFIX)/bin; gcp i386-pc-solaris2.8-gcj gcj)
+	$(_DBG)(cd $(IPREFIX)/bin; gcp i386-pc-solaris2.8-gfortran gfortran)
 	$(_DBG)(cd $(APREFIX)/bin; grm -f *gcc *c++ g++ gcj gfortran)
 	$(_DBG)(cd $(APREFIX)/bin; \
-		gcp sparc-sun-solaris2.10-gcc-4.3.3 sparc-sun-solaris2.10-gcc)
-	$(_DBG)(cd $(APREFIX)/bin; gcp sparc-sun-solaris2.10-gcc-4.3.3 gcc)
+		gcp i386-pc-solaris2.10-gcc-4.3.3 i386-pc-solaris2.10-gcc)
+	$(_DBG)(cd $(APREFIX)/bin; gcp i386-pc-solaris2.10-gcc-4.3.3 gcc)
 	$(_DBG)(cd $(APREFIX)/bin; \
-		gcp sparc-sun-solaris2.10-g++ sparc-sun-solaris2.10-c++)
-	$(_DBG)(cd $(APREFIX)/bin; gcp sparc-sun-solaris2.10-g++ g++)
-	$(_DBG)(cd $(APREFIX)/bin; gcp sparc-sun-solaris2.10-g++ c++)
-	$(_DBG)(cd $(APREFIX)/bin; gcp sparc-sun-solaris2.10-gcj gcj)
-	$(_DBG)(cd $(APREFIX)/bin; gcp sparc-sun-solaris2.10-gfortran gfortran)
+		gcp i386-pc-solaris2.10-g++ i386-pc-solaris2.10-c++)
+	$(_DBG)(cd $(APREFIX)/bin; gcp i386-pc-solaris2.10-g++ g++)
+	$(_DBG)(cd $(APREFIX)/bin; gcp i386-pc-solaris2.10-g++ c++)
+	$(_DBG)(cd $(APREFIX)/bin; gcp i386-pc-solaris2.10-gcj gcj)
+	$(_DBG)(cd $(APREFIX)/bin; gcp i386-pc-solaris2.10-gfortran gfortran)
 	$(_DBG)$(MAKECOOKIE)
 
 merge-i386-files:
 	@echo "[===== Merging isa-i386 =====]"
 	$(_DBG)(cd $(I386_BASE); /usr/bin/pax -rw -v $(MPREFIX)/bin $(PKGROOT))
-	$(_DBG)(cd $(IPREFIX)/bin; \
-			/usr/bin/pax -rw -v *solaris2* $(PPREFIX)/bin/i386)
+	$(_DBG)(cd $(PPREFIX)/bin; gmv *solaris2* $(PPREFIX)/bin/i386)
 	$(_DBG)(cd $(I386_BASE); /usr/bin/pax -rw -v $(MPREFIX)/include $(PKGROOT))
 	$(_DBG)(cd $(I386_BASE); /usr/bin/pax -rw -v $(MPREFIX)/info $(PKGROOT))
 	$(_DBG)(cd $(I386_BASE); /usr/bin/pax -rw -v $(MPREFIX)/man $(PKGROOT))
@@ -73,48 +73,46 @@ merge-amd64-files:
 	$(_DBG)$(MAKECOOKIE)
 
 ifeq ($(shell uname -p), i386)
-EXTRA_PKGFILES_CSWgcc4core  = l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gcc
-EXTRA_PKGFILES_CSWgcc4core += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/cpp
-
-EXTRA_PKGFILES_CSWgcc4g++  = l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/c++
-EXTRA_PKGFILES_CSWgcc4g++ += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/g++
-
-EXTRA_PKGFILES_CSWgcc4gfortran = l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gfortran
-
-EXTRA_PKGFILES_CSWgcc4java  = l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gcj
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gcov
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gccbug
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/addr2name.awk
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gc-analyze
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gcjh
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gjarsigner
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/grmic
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gjavah
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/grmid
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/jcf-dump
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gkeytool
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/grmiregistry
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/jv-convert
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gij
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gnative2ascii
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gserialver
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gappletviewer
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gcj-dbtool
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gjar
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gorbd
-EXTRA_PKGFILES_CSWgcc4java += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gtnameserv
-
-EXTRA_PKGFILES_CSWgcc4ada  = l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gnat
-EXTRA_PKGFILES_CSWgcc4ada += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gnatbind
-EXTRA_PKGFILES_CSWgcc4ada += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gnatbl
-EXTRA_PKGFILES_CSWgcc4ada += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gnatchop
-EXTRA_PKGFILES_CSWgcc4ada += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gnatclean
-EXTRA_PKGFILES_CSWgcc4ada += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gnatfind
-EXTRA_PKGFILES_CSWgcc4ada += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gnatkr
-EXTRA_PKGFILES_CSWgcc4ada += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gnatlink
-EXTRA_PKGFILES_CSWgcc4ada += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gnatls
-EXTRA_PKGFILES_CSWgcc4ada += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gnatmake
-EXTRA_PKGFILES_CSWgcc4ada += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gnatname
-EXTRA_PKGFILES_CSWgcc4ada += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gnatprep
-EXTRA_PKGFILES_CSWgcc4ada += l none /opt/csw/bin/isaexec=/opt/csw/gcc4/bin/gnatxref
+ISAEXEC_DIRS   = /opt/csw/gcc4/bin
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gcc
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gcov
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gccbug
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gfortran
+ISAEXEC_FILES += /opt/csw/gcc4/bin/c++
+ISAEXEC_FILES += /opt/csw/gcc4/bin/g++
+ISAEXEC_FILES += /opt/csw/gcc4/bin/cpp
+ISAEXEC_FILES += /opt/csw/gcc4/bin/addr2name.awk
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gc-analyze
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gcjh
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gjarsigner
+ISAEXEC_FILES += /opt/csw/gcc4/bin/grmic
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gjavah
+ISAEXEC_FILES += /opt/csw/gcc4/bin/grmid
+ISAEXEC_FILES += /opt/csw/gcc4/bin/jcf-dump
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gkeytool
+ISAEXEC_FILES += /opt/csw/gcc4/bin/grmiregistry
+ISAEXEC_FILES += /opt/csw/gcc4/bin/jv-convert
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gcj
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gij
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnative2ascii
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gserialver
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gappletviewer
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gcj-dbtool
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gjar
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gorbd
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gtnameserv
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnat
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnatbind
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnatbl
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnatchop
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnatclean
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnatfind
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnative2ascii
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnatkr
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnatlink
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnatls
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnatmake
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnatname
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnatprep
+ISAEXEC_FILES += /opt/csw/gcc4/bin/gnatxref
 endif
