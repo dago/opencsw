5f1bbc64d4ca58b20b23bf1e88d068023ff45f45
valholla <valholla@d3b55034-1cff-0310-a425-aefe953e1e90>
2009-05-14 16:57:30 +0000

    define dependencies for packages

---
+++ csw/mgar/pkg/php4/trunk/files/depend.mk
@@ -0,0 +1,29 @@
+#
+# Define Package Dependencies
+#
+
+REQUIRED_PKGS_CSWphp4bcmath = CSWphp4cgi
+REQUIRED_PKGS_CSWphp4bz2 = CSWphp4cgi CSWbzip2
+REQUIRED_PKGS_CSWphp4calendar = CSWphp4cgi 
+REQUIRED_PKGS_CSWphp4cgi = CSWbdb44 CSWexpat
+REQUIRED_PKGS_CSWphp4curl = CSWphp4cgi CSWcurlrt CSWlibidn CSWoldaprt CSWosslrt CSWzlib
+REQUIRED_PKGS_CSWphp4dba = CSWphp4cgi CSWbdb44 CSWgdbm
+REQUIRED_PKGS_CSWphp4domxml = CSWphp4cgi CSWiconv CSWlibxml2 CSWlibxslt CSWzlib
+REQUIRED_PKGS_CSWphp4gd = CSWphp4cgi CSWgd CSWjpeg CSWpng CSWzlib
+REQUIRED_PKGS_CSWphp4gettext = CSWphp4cgi CSWggettextrt
+REQUIRED_PKGS_CSWphp4gmp = CSWphp4cgi CSWlibgmp
+REQUIRED_PKGS_CSWphp4iconv = CSWphp4cgi 
+REQUIRED_PKGS_CSWphp4imap = CSWphp4cgi CSWimaprt CSWkrb5lib CSWosslrt
+REQUIRED_PKGS_CSWphp4ldap = CSWphp4cgi CSWoldaprt
+REQUIRED_PKGS_CSWphp4mbstring = CSWphp4cgi 
+REQUIRED_PKGS_CSWphp4mcal = CSWphp4cgi
+REQUIRED_PKGS_CSWphp4mssql = CSWphp4cgi CSWfreetds
+REQUIRED_PKGS_CSWphp4mysql = CSWphp4cgi CSWmysql5rt
+REQUIRED_PKGS_CSWphp4ncurses = CSWphp4cgi CSWncurses
+REQUIRED_PKGS_CSWphp4odbc = CSWphp4cgi CSWunixodbc
+REQUIRED_PKGS_CSWphp4openssl = CSWphp4cgi CSWkrb5lib CSWosslrt
+REQUIRED_PKGS_CSWphp4pgsql = CSWphp4cgi CSWlibpq
+REQUIRED_PKGS_CSWphp4zlib = CSWphp4cgi CSWzlib
+REQUIRED_PKGS_CSWap2modphp4 = CSWphp4cgi CSWbdb44 CSWexpat CSWap2prefork
+REQUIRED_PKGS_CSWmodphp4 = CSWphp4cgi CSWbdb44 CSWexpat CSWapache
+REQUIRED_PKGS_CSWmodphp4core = CSWphp4cgi
---
+++ csw/mgar/pkg/php4/trunk/files/pkgfiles.mk
@@ -0,0 +1,32 @@
+# 
+# define the files in each Package
+#
+
+PKGFILES_CSWphp4bcmath = .*/extensions/.*/bcmath.*
+PKGFILES_CSWphp4bz2 = .*/extensions/.*/bz2.*
+PKGFILES_CSWphp4calendar = .*/extensions/.*/calendar.*
+PKGFILES_CSWphp4curl = .*/extensions/.*/curl.*
+PKGFILES_CSWphp4dba = .*/extensions/.*/dba.*
+PKGFILES_CSWphp4domxml = .*/extensions/.*/domxml.*
+PKGFILES_CSWphp4gd = .*/extensions/.*/gd.*
+PKGFILES_CSWphp4gettext = .*/extensions/.*/gettext.*
+PKGFILES_CSWphp4gmp = .*/extensions/.*/gmp.*
+PKGFILES_CSWphp4iconv  = .*/extensions/.*/iconv.*
+PKGFILES_CSWphp4iconv += .*/include/php/ext/iconv.*
+PKGFILES_CSWphp4imap = .*/extensions/.*/imap.*
+PKGFILES_CSWphp4ldap = .*/extensions/.*/ldap.*
+PKGFILES_CSWphp4mbstring  = .*/extensions/.*/mbstring.*
+PKGFILES_CSWphp4mbstring += .*/include/php/ext/mbstring.*
+PKGFILES_CSWphp4mcal = .*/extensions/.*/mcal.*
+PKGFILES_CSWphp4mssql = .*/extensions/.*/mssql.*
+PKGFILES_CSWphp4mysql = .*/extensions/.*/mysql.*
+PKGFILES_CSWphp4ncurses = .*/extensions/.*/ncurses.*
+PKGFILES_CSWphp4odbc = .*/extensions/.*/odbc.*
+PKGFILES_CSWphp4openssl = .*/extensions/.*/openssl.*
+PKGFILES_CSWphp4pgsql  = .*/extensions/.*/pgsql.*
+PKGFILES_CSWphp4pgsql += .*/include/php/ext/pgsql.*
+PKGFILES_CSWphp4zlib = .*/extensions/.*/zlib.*
+PKGFILES_CSWap2modphp4 = .*/apache2/.*
+PKGFILES_CSWmodphp4 = .*/apache/.*
+PKGFILES_CSWmodphp4core = /opt/csw/php4/README-mod_php4_core
+#PKGFILES_CSWphp4cgi catchall
