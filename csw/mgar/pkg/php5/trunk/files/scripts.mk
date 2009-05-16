62c162891e3e1c4acaa27ea899f6d83785841fa5
valholla <valholla@d3b55034-1cff-0310-a425-aefe953e1e90>
2009-05-16 14:38:21 +0000

    fix checkusms

--- csw/mgar/pkg/php5/trunk/files/scripts.mk
+++ csw/mgar/pkg/php5/trunk/files/scripts.mk
@@ -24,14 +24,15 @@ post-install-modulated: $(PI_SCRIPTS)
 
 install-extras:
 	@echo "[====> Fixing Admin Scripts <====]"
+	gcp $(DOWNLOADDIR)/php.ini.CSW $(DOWNLOADDIR)/php.ini.CSW.fixed
+	perl -i -pne "s|x_PHPEXTDIR_x|_PHPEXTDIR_|" $(DOWNLOADDIR)/php.ini.CSW.fixed
 	perl -i -pne "s|_PHPINIFILE_|$(call _get_php_ini_path,$(DESTDIR))/php.ini|" `gfind $(DOWNLOADDIR) -type f -print`
 	perl -i -pne "s|_PHPLIBDIR_|$(call _get_php_ini_path,$(DESTDIR))|" `gfind $(DOWNLOADDIR) -type f -print`
 	perl -i -pne "s|_PHPBINDIR_|$(call _get_php_prefix,$(DESTDIR))/bin|" `gfind $(DOWNLOADDIR) -type f -print`
 	perl -i -pne "s|_PHPETCDIR_|$(call _get_php_prefix,$(DESTDIR))/etc|" `gfind $(DOWNLOADDIR) -type f -print`
+	perl -i -pne 's|_PHPEXTDIR_|$(shell $(DESTDIR)$(call _get_php_prefix,$(DESTDIR))/bin/php-config --extension-dir)|' `gfind $(DOWNLOADDIR) -type f -print`
 	@echo "[====> Installing Extra Files <====]"
 	ginstall -m 0755 $(DOWNLOADDIR)/phpext $(DESTDIR)$(call _get_php_prefix,$(DESTDIR))/bin
-	gcp $(DOWNLOADDIR)/php.ini.CSW $(DOWNLOADDIR)/php.ini.CSW.fixed
-	perl -i -pne 's|_PHPEXTDIR_|$(shell $(DESTDIR)$(call _get_php_prefix,$(DESTDIR))/bin/php-config --extension-dir)|' $(DOWNLOADDIR)/php.ini.CSW.fixed
 	gcp $(DOWNLOADDIR)/php.ini.CSW.fixed $(DESTDIR)$(call _get_php_ini_path,$(DESTDIR))/php.ini.CSW
 	gmv $(DESTDIR)$(call _get_php_prefix,$(DESTDIR))/etc/pear.conf $(DESTDIR)$(call _get_php_prefix,$(DESTDIR))/etc/pear.conf.CSW
 	gchmod 0644 $(DESTDIR)$(call _get_php_ini_path,$(DESTDIR))/php.ini.CSW
