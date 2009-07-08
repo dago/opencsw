d957fbdd096a570d1c04b086360f3d5feb710485
wbonnet <wbonnet@d3b55034-1cff-0310-a425-aefe953e1e90>
2009-07-08 12:20:50 +0000

    Fix path removal script

--- csw/mgar/pkg/firefox/branches/firefox3-packaging/removeBuildPathFromJar.sh
+++ csw/mgar/pkg/firefox/branches/firefox3-packaging/removeBuildPathFromJar.sh
@@ -1,4 +1,4 @@
-#!/opt/csw/bin/bash 
+#!/opt/csw/bin/bash -x
 
 if [ -d ~/.tempextractjar ] ; 
 then 
@@ -10,11 +10,11 @@ BACKUP_OLD_DIR=`pwd`
 export REPLACEDIR=$2/$3/$4
 
 cd ~/.tempextractjar
-jar xvf $1
+jar xvf $BACKUP_OLD_DIR/$1
 
 gfind . -name "*.js" -exec bash -x -c " mv {} {}.temp && gsed -e  s_\$REPLACEDIR\_\_g {}.temp > {} && rm {}.temp"  \; 
 
 rm $1
-jar cvf $1 *
+jar cvf $BACKUP_OLD_DIR/$1 *
 
 cd $BACKUP_OLD_DIR
