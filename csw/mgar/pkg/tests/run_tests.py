14d263ae8c44ddfd4e0496d6c8636d2c3f6630ee
wahwah <wahwah@d3b55034-1cff-0310-a425-aefe953e1e90>
2010-01-24 07:18:53 +0000

    mGAR v2: tests, separated the library and the executable, better API for variables.

---
+++ csw/mgar/pkg/tests/run_tests.py
@@ -0,0 +1,26 @@
+# $Id$
+
+import unittest
+import gartest
+
+class ExampleEndToEndTest(unittest.TestCase):
+  """An example end-to-end test of GAR."""
+
+  def testPkginfoName(self):
+    """Checks that the GARNAME makes it to the NAME in pkginfo."""
+    mybuild = gartest.GarBuild()
+    mybuild.SetGarVariable("GARNAME", "foo")
+    mybuild.AddInstallFile("/opt/csw/share/foo", "bar!\n")
+    mybuild.WriteGarFiles()
+    self.assertEquals(0, mybuild.Build())
+    pkg = mybuild.GetFirstBuiltPackage()
+    pkginfo = pkg.GetParsedPkginfo()
+    # By default, the garname should be used to create the catalog name, which
+    # in turn ends up in the NAME field in pkginfo.
+    self.assertTrue(pkginfo["NAME"].startswith("foo"))
+
+
+if __name__ == '__main__':
+  unittest.main()
+
+# vim:set ts=2 sts=2 sw=2 expandtab:
