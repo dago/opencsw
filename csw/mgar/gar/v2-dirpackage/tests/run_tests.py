3745f6095650084401092cfa07a94623e6f52c17
wahwah <wahwah@d3b55034-1cff-0310-a425-aefe953e1e90>
2010-01-25 11:26:34 +0000

    mGAR v2-dirpackage: Static build tests.

--- csw/mgar/gar/v2-dirpackage/tests/run_tests.py
+++ csw/mgar/gar/v2-dirpackage/tests/run_tests.py
@@ -2,6 +2,8 @@
 # $Id$
 
 import unittest
+import sys
+sys.path.append("../lib/python")
 import gartest
 
 class ExampleEndToEndTest(unittest.TestCase):
@@ -9,7 +11,7 @@ class ExampleEndToEndTest(unittest.TestCase):
 
   def testPkginfoName(self):
     """Checks that the GARNAME makes it to the NAME in pkginfo."""
-    mybuild = gartest.GarBuild()
+    mybuild = gartest.DynamicGarBuild()
     mybuild.SetGarVariable("GARNAME", "foo")
     mybuild.AddInstallFile("/opt/csw/share/foo", "bar!\n")
     mybuild.WriteGarFiles()
@@ -19,6 +21,21 @@ class ExampleEndToEndTest(unittest.TestCase):
     # By default, the garname should be used to create the catalog name, which
     # in turn ends up in the NAME field in pkginfo.
     self.assertTrue(pkginfo["NAME"].startswith("foo"))
+
+
+class StaticBuildTestExample(unittest.TestCase):
+  """An example of a static build.
+
+  This uses a static directory where GAR can be called.
+  """
+
+  def testLooseFiles(self):
+    mybuild = gartest.StaticGarBuild("static/example")
+    mybuild.Build()
+    pkg = mybuild.GetFirstBuiltPackage()
+    pkginfo = pkg.GetParsedPkginfo()
+    # This part would often use "self.assertEqual(<expected>, <actual>)"
+    self.assertTrue(pkginfo["NAME"].startswith("loose"))
 
 
 if __name__ == '__main__':
