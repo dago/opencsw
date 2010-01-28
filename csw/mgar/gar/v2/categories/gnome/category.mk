f080953848f7f8e1b6f1efa3f80d4a39e473f137
wahwah <wahwah@d3b55034-1cff-0310-a425-aefe953e1e90>
2010-01-28 12:07:40 +0000

    mGAR v2: gnome category, adding useful defaults

--- csw/mgar/gar/v2/categories/gnome/category.mk
+++ csw/mgar/gar/v2/categories/gnome/category.mk
@@ -1,1 +1,13 @@
+# pkg-config options
+EXTRA_PKG_CONFIG_PATH += /opt/csw/X11/lib/pkgconfig
+
+MSGFMT= /opt/csw/bin/gmsgfmt
+MSGMERGE= /opt/csw/bin/gmsgmerge
+XGETTEXT = /opt/csw/bin/gxgettext
+GETTEXT = /opt/csw/bin/ggettext
+export MSGMERGE
+export MSGFMT
+export XGETTEXT
+export GETTEXT
+
 include gar/gar.mk
