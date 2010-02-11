d0b1667ad4548193f68e7753643a8e2b00426a57
wahwah <wahwah@d3b55034-1cff-0310-a425-aefe953e1e90>
2010-02-11 23:58:44 +0000

    mGAR v2: checkpkg, added a test case for two overrides

--- csw/mgar/gar/v2/tests/overrides_test.py
+++ csw/mgar/gar/v2/tests/overrides_test.py
@@ -9,12 +9,13 @@ import gartest
 class OverridesUnitTest(unittest.TestCase):
   """Tests CHECKPKG_OVERRIDES support."""
 
-  def testPkginfoName(self):
-    """Checks that the GARNAME makes it to the NAME in pkginfo."""
+  def testOneOverride(self):
+    """Checks that CHECKPKG_OVERRIDES variable creates an override."""
     mybuild = gartest.DynamicGarBuild()
     mybuild.SetGarVariable("GARNAME", "overrides-test")
     mybuild.SetGarVariable("CATALOGNAME", "overrides_test")
-    mybuild.SetGarVariable("CHECKPKG_OVERRIDES", "CSWoverrides-test|example-tag|example-parameter")
+    mybuild.SetGarVariable("CHECKPKG_OVERRIDES",
+                           "CSWoverrides-test|example-tag|example-parameter")
     mybuild.WriteGarFiles()
     self.assertEquals(0, mybuild.Build())
     pkg = mybuild.GetFirstBuiltPackage()
@@ -23,3 +24,22 @@ class OverridesUnitTest(unittest.TestCase):
     self.assertEqual(expected, pkg.GetFileContent(overr_file))
     overrides = pkg.GetOverrides()
     self.assertEqual(1, len(overrides))
+
+  def testTwoOverriders(self):
+    """Checks that CHECKPKG_OVERRIDES variable creates overrides."""
+    mybuild = gartest.DynamicGarBuild()
+    mybuild.SetGarVariable("GARNAME", "overrides-test")
+    mybuild.SetGarVariable("CATALOGNAME", "overrides_test")
+    mybuild.SetGarVariable(
+        "CHECKPKG_OVERRIDES",
+        ("CSWoverrides-test|example-tag-1|example-parameter-1 "
+         "CSWoverrides-test|example-tag-2|example-parameter-2"))
+    mybuild.WriteGarFiles()
+    self.assertEquals(0, mybuild.Build())
+    pkg = mybuild.GetFirstBuiltPackage()
+    overr_file = "/opt/csw/share/checkpkg/overrides/overrides_test"
+    expected = ('CSWoverrides-test: example-tag-1 example-parameter-1\n'
+                'CSWoverrides-test: example-tag-2 example-parameter-2\n')
+    self.assertEqual(expected, pkg.GetFileContent(overr_file))
+    overrides = pkg.GetOverrides()
+    self.assertEqual(2, len(overrides))
