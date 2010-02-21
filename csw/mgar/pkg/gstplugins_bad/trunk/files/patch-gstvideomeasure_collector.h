59d0eea2253d6d144253681a30f775007e36bd9c
bensons <bensons@d3b55034-1cff-0310-a425-aefe953e1e90>
2010-02-21 15:38:47 +0000

    gstplugins_bad: initial GAR recipe

---
+++ csw/mgar/pkg/gstplugins_bad/trunk/files/patch-gstvideomeasure_collector.h
@@ -0,0 +1,25 @@
+--- gst-plugins-bad-0.10.17.orig/gst/videomeasure/gstvideomeasure_collector.h	2009-10-12 12:23:35.000000000 +0200
++++ gst-plugins-bad-0.10.17/gst/videomeasure/gstvideomeasure_collector.h	2010-02-21 16:23:40.000000000 +0100
+@@ -29,17 +29,11 @@
+ typedef struct _GstMeasureCollectorClass GstMeasureCollectorClass;
+ 
+ #define GST_TYPE_MEASURE_COLLECTOR            (gst_measure_collector_get_type())
+-#define GST_MEASURE_COLLECTOR(obj)                                             \
+-    (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_MEASURE_COLLECTOR,              \
+-    GstMeasureCollector))
+-#define GST_IS_MEASURE_COLLECTOR(obj)         \
+-    (G_TYPE_CHECK_INSTANCE_TYPE((obj), GST_TYPE_MEASURE_COLLECTOR))
+-#define GST_MEASURE_COLLECTOR_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass),\
+-    GST_TYPE_MEASURE_COLLECTOR, GstMeasureCollectorClass))
+-#define GST_IS_MEASURE_COLLECTOR_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass),\
+-    GST_TYPE_MEASURE_COLLECTOR))
+-#define GST_MEASURE_COLLECTOR_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj),\
+-    GST_TYPE_MEASURE_COLLECTOR, GstMeasureCollectorClass))
++#define GST_MEASURE_COLLECTOR(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_MEASURE_COLLECTOR,              GstMeasureCollector))
++#define GST_IS_MEASURE_COLLECTOR(obj)          (G_TYPE_CHECK_INSTANCE_TYPE((obj), GST_TYPE_MEASURE_COLLECTOR))
++#define GST_MEASURE_COLLECTOR_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), GST_TYPE_MEASURE_COLLECTOR, GstMeasureCollectorClass))
++#define GST_IS_MEASURE_COLLECTOR_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), GST_TYPE_MEASURE_COLLECTOR))
++#define GST_MEASURE_COLLECTOR_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), GST_TYPE_MEASURE_COLLECTOR, GstMeasureCollectorClass))
+ 
+ typedef enum {
+   GST_MEASURE_COLLECTOR_0 = 0,
---
+++ csw/mgar/pkg/gstplugins_bad/trunk/files/patch-gstvideomeasure_ssim.h
@@ -0,0 +1,24 @@
+--- gst-plugins-bad-0.10.17.orig/gst/videomeasure/gstvideomeasure_ssim.h	2009-10-12 12:23:35.000000000 +0200
++++ gst-plugins-bad-0.10.17/gst/videomeasure/gstvideomeasure_ssim.h	2010-02-21 16:23:33.000000000 +0100
+@@ -37,16 +37,11 @@
+ 
+ 
+ #define GST_TYPE_SSIM            (gst_ssim_get_type())
+-#define GST_SSIM(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj),            \
+-    GST_TYPE_SSIM,GstSSim))
+-#define GST_IS_SSIM(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj),            \
+-    GST_TYPE_SSIM))
+-#define GST_SSIM_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass) ,            \
+-    GST_TYPE_SSIM,GstSSimClass))
+-#define GST_IS_SSIM_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass) ,            \
+-    GST_TYPE_SSIM))
+-#define GST_SSIM_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj) ,            \
+-    GST_TYPE_SSIM,GstSSimClass))
++#define GST_SSIM(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), GST_TYPE_SSIM,GstSSim))
++#define GST_IS_SSIM(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), GST_TYPE_SSIM))
++#define GST_SSIM_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass) , GST_TYPE_SSIM,GstSSimClass))
++#define GST_IS_SSIM_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass) , GST_TYPE_SSIM))
++#define GST_SSIM_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj) , GST_TYPE_SSIM,GstSSimClass))
+ 
+ typedef struct _GstSSim             GstSSim;
+ typedef struct _GstSSimClass        GstSSimClass;
