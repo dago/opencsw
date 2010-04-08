086cb6cd2ca1d2244b611186725096f14b8b1599
skayser <skayser@d3b55034-1cff-0310-a425-aefe953e1e90>
2010-04-08 21:22:34 +0000

    tmpwatch: add changelog.CSW, cleanups for first pkg release

--- csw/mgar/pkg/tmpwatch/trunk/files/stpcpy.c
+++
@@ -1,9 +0,0 @@
-char *                                                                                    
-stpcpy(char *a,const char *b)                                                             
-{                                                                                         
-    while( *b )                                                                           
-    *a++ = *b++;                                                                          
-    *a = 0;                                                                               
-                                                                                          
-    return (char*)a;                                                                      
-}                     
