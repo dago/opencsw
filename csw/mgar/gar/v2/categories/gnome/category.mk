b8ab703bc1c31282395001d002ccf30af4b45871
bdwalton <bdwalton@d3b55034-1cff-0310-a425-aefe953e1e90>
2010-06-26 00:39:31 +0000

    gar/v2: drop CSW X11 values from the gnome category

--- csw/mgar/gar/v2/categories/gnome/category.mk
+++ csw/mgar/gar/v2/categories/gnome/category.mk
@@ -1,6 +1,3 @@
-# pkg-config options
-EXTRA_PKG_CONFIG_DIRS += /opt/csw/X11/lib
-
 MSGFMT= /opt/csw/bin/gmsgfmt
 MSGMERGE= /opt/csw/bin/gmsgmerge
 XGETTEXT = /opt/csw/bin/gxgettext
@@ -11,9 +8,7 @@ export XGETTEXT
 export GETTEXT
 
 # Perhaps there is a category-level variable set?
-EXTRA_INC += /opt/csw/X11/include
 EXTRA_INC += /usr/X11/include
 EXTRA_INC += /usr/openwin/share/include
-EXTRA_LIB += /opt/csw/X11/lib
 
 include gar/gar.mk
