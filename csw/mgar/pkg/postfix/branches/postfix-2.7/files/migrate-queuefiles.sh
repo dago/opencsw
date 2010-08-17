fc4f169e8c1f2d718cf3df7f6de6f71d30c62cd2
skayser <skayser@d3b55034-1cff-0310-a425-aefe953e1e90>
2010-08-17 12:17:20 +0000

    postfix 2.7: comment build peculiarities, add confirmation step to migrate-queuefiles.sh script

--- csw/mgar/pkg/postfix/branches/postfix-2.7/files/migrate-queuefiles.sh
+++ csw/mgar/pkg/postfix/branches/postfix-2.7/files/migrate-queuefiles.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 #
-# move-spool.sh:
+# migrate-queuefiles.sh:
 #  helper script to move possible queue files from the CSWpostfix 2.4.x
 #  spool location to the CSWpostfix >= 2.6.x spool location.
 #
 # $Id$
 
-
 SPOOL_OLD=/opt/csw/var/spool/postfix
 SPOOL_NEW=/var/opt/csw/spool/postfix
 QDIRS="incoming active deferred corrupt hold bounce defer trace"
+
+if [ "$1" != "-y" ]; then
+  echo "This helper script will move leftover postfix email queue files from "
+  echo 
+  echo "  $SPOOL_OLD   to   $SPOOL_NEW"
+  echo
+  echo "Please make sure to understand what it does, backup your queue files"
+  echo "in advance, and re-run this script with -y to start the process."
+  exit 1
+fi
 
 [ -x /usr/bin/zonename ] && ZONEOPT="-z `/usr/bin/zonename`"
 if pgrep $ZONEOPT -lf /opt/csw/libexec/postfix/master; then
@@ -19,7 +28,7 @@ fi
 
 for qdir in $QDIRS; do
     [ -d ${SPOOL_OLD}/$qdir ] || continue
-	echo "Moving from ${SPOOL_OLD}/$qdir to ${SPOOL_NEW}/$qdir"
+	echo "Moving files from ${SPOOL_OLD}/$qdir to ${SPOOL_NEW}/$qdir"
 	cd ${SPOOLDIR_NEW}/$qdir && \
 		find ${SPOOLDIR_OLD}/$qdir -type f -exec mv '{}' . +
 done
