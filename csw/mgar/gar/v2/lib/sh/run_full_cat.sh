f00a3fc008dfa0cc16c3655cb5ed42711a0b61fb
wahwah <wahwah@d3b55034-1cff-0310-a425-aefe953e1e90>
2010-10-26 09:34:57 +0000

    mGAR v2: checkpkg, updates to the script that runs against the whole catalog.

--- csw/mgar/gar/v2/lib/sh/run_full_cat.sh
+++ csw/mgar/gar/v2/lib/sh/run_full_cat.sh
@@ -1,8 +1,24 @@
 #!/opt/csw/bin/bash
+#
+# A small script to run checkpkg against the whole catalog.  The results are
+# stored in a sqlite database in ~/.checkpkg.
+#
+# Architecture
+# sparc, i386
+arch=sparc
 
-# A small script to run checkpkg against the whole catalog
+# Solaris version
+# 5.9, 5.10
+ver=5.9
 
-c="/home/mirror/opencsw/current/sparc/5.9"
+# OpenCSW release
+# current, stable, testing, unstable
+release=current
+
+c="/home/mirror/opencsw/${release}/${arch}/${ver}"
+readonly c
+
+time \
 bin/checkpkg \
   -q \
   -M "${c}/catalog" \
