ab83caca1f2adbff0f0be3ac3418a39be2b266d6
skayser <skayser@d3b55034-1cff-0310-a425-aefe953e1e90>
2010-11-30 23:00:34 +0000

    gar/v2-noexternals: drop the requirement for the $GARDIR env variable

--- csw/mgar/gar/v2-noexternals/category.mk
+++ csw/mgar/gar/v2-noexternals/category.mk
@@ -1,13 +1,12 @@
 # vim: ft=make ts=4 sw=4 noet
-# This makefile is to be included from Makefiles in each category
-# directory.
+#
+# This Makefile is the main entry point to GAR and is included by
+# each package build description. As such, the file name 'category.mk'
+# is slightly misleading and could be subject to future change.
+#
 
-# This is needed by the pkg build approach with gar/ in each pkg build
-# directory and is overriden by the "mgar" wrapper which pre-sets GARDIR
-# with an absolute path. If we could determine the full path of
-# _this_ file's directory, we could use it for both approaches and
-# there would be no need to pre-set it for "mgar".
-GARDIR ?= $(CURDIR)/gar
+# Determine this file's directory, i.e. the GAR base directory
+GARDIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
 ifeq (,$(wildcard $(GARDIR)/categories/$(CATEGORIES)/category.mk))
   $(error The category '$(CATEGORIES)' is invalid. Valid categories are: $(patsubst $(GARDIR)/categories/%,%,$(wildcard $(GARDIR)/categories/*)))
