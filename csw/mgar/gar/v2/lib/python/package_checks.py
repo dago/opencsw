796bee19304a8fbd202485c9a055c891974954e9
wahwah <wahwah@d3b55034-1cff-0310-a425-aefe953e1e90>
2010-12-16 21:27:52 +0000

    checkpkg: CSWcswclassutils is deprecated

--- csw/mgar/gar/v2/lib/python/package_checks.py
+++ csw/mgar/gar/v2/lib/python/package_checks.py
@@ -60,6 +60,12 @@ OBSOLETE_DEPS = {
               "shared library packages.",
       "url": "http://wiki.opencsw.org/packaging-shared-libraries",
     }
+    "CSWcswclassutils": {
+      "hint": "CSWcswclassutils is deprecated, please depend on specific "
+              "CSWcas-* packages.",
+              "url": ("http://lists.opencsw.org/pipermail/maintainers/"
+                      "2010-October/012862.html"),
+    }
 }
 ARCH_RE = re.compile(r"(sparcv(8|9)|i386|amd64)")
 EMAIL_RE = re.compile(r"^.*@opencsw.org$")
