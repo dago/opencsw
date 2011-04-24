26694ff663829e9d8644506e97afccf3ddaaa551
wahwah <wahwah@d3b55034-1cff-0310-a425-aefe953e1e90>
2011-04-24 10:01:48 +0000

    pkg/cswclassutils/trunk: Potentially fixed a bug with tar invocations

--- csw/mgar/pkg/cswclassutils/trunk/files/cswmigrateconf_test.sh
+++ csw/mgar/pkg/cswclassutils/trunk/files/cswmigrateconf_test.sh
@@ -6,20 +6,21 @@ set -e
 
 readonly CAS_DIR="files"
 readonly CAS_UNDER_TEST="CSWcswclassutils.i.cswmigrateconf"
-readonly TMP_DIR="$(gmktemp -d castest.XXXXXXXXXX)"
-readonly SRC_DIR="${TMP_DIR}/src"
-PKG_INSTALL_ROOT="${TMP_DIR}/root"
+# readonly WORK_DIR="$(gmktemp -d /tmp/castest.XXXXXXXXXX)"
+readonly WORK_DIR="$(gmktemp -d castest.XXXXXXXXXX)"
+readonly SRC_DIR="${WORK_DIR}/src"
+PKG_INSTALL_ROOT="${WORK_DIR}/root"
 readonly OLD_CONFDIR_ABS="${PKG_INSTALL_ROOT}/opt/csw/etc"
 readonly NEW_CONFDIR_ABS="${PKG_INSTALL_ROOT}/etc/opt/csw"
 
 cleanup() {
-tree "${TMP_DIR}"
+tree "${WORK_DIR}"
 echo "To clean up:"
-echo "rm -rf \"${TMP_DIR}\""
+echo "rm -rf \"${WORK_DIR}\""
 read -p "Clean? [y/n] > " ANSWER
 if [[ "${ANSWER}" == y ]]
 then
-  rm -rf "${TMP_DIR}"
+  rm -rf "${WORK_DIR}"
 else
   echo "Not removing."
 fi
@@ -27,18 +28,18 @@ fi
 
 trap cleanup EXIT
 
-echo "Working in '${TMP_DIR}'"
+echo "Working in '${WORK_DIR}'"
 mkdir -p "${PKG_INSTALL_ROOT}"
 mkdir -p "${SRC_DIR}"
 
 # Place files
 echo "MIGRATE_FILES=\"foo.conf bar.d\"" > "${SRC_DIR}/foo"
-echo "MIGRATE_FILES=\"bar.d\"" > "${SRC_DIR}/foo"
 mkdir -p "${OLD_CONFDIR_ABS}"
 echo "foo: bar" > "${OLD_CONFDIR_ABS}/foo.conf"
 
 mkdir -p "${OLD_CONFDIR_ABS}/bar.d"
 echo "I am the bar daemon." > "${OLD_CONFDIR_ABS}/bar.d/bar.conf"
+echo "I have two configuration files." > "${OLD_CONFDIR_ABS}/bar.d/baz.conf"
 
 # And a directory
 
