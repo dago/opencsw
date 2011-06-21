33a441f1e27adec98baad281582d441fa8204f83
skayser <skayser@d3b55034-1cff-0310-a425-aefe953e1e90>
2011-06-21 21:48:27 +0000

    gar/v2: add sanity checks for required make features

--- csw/mgar/gar/v2/category.mk
+++ csw/mgar/gar/v2/category.mk
@@ -5,7 +5,20 @@
 # is slightly misleading and could be subject to future change.
 #
 
-# Determine this file's directory, i.e. the GAR base directory
+# Safety check, we need GNU make >= 3.81 for the magic further below,
+# more precisly: 3.80 for $(MAKEFILE_LIST), 3.81 for $(lastword ...)
+# c.f.: http://cvs.savannah.gnu.org/viewvc/make/NEWS?root=make&view=markup
+ifeq (,$(and $(MAKEFILE_LIST),$(lastword test)))
+define error_msg
+GNU make >= 3.81 required.
+Try "pkgutil -i gmake" and prepend /opt/csw/bin to your $$PATH.
+endef
+  $(error $(error_msg))
+endif
+
+# Determine this file's directory, i.e. the GAR base directory. We
+# need to do this dynamically as we don't want to rely on the presence
+# of gar/ symlinks in each build directory.
 GARDIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
 ifeq (,$(wildcard $(GARDIR)/categories/$(CATEGORIES)/category.mk))
