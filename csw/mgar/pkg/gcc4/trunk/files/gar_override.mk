c12912d869ed095887e7ec778e021397fa9160c4
dmichelsen <dmichelsen@d3b55034-1cff-0310-a425-aefe953e1e90>
2011-08-12 09:44:41 +0000

    gcc4: Rip out unnecessary definitions

--- csw/mgar/pkg/gcc4/trunk/files/gar_override.mk
+++ csw/mgar/pkg/gcc4/trunk/files/gar_override.mk
@@ -1,43 +1,47 @@
 
-COOKIEDIR     = $(COOKIEROOTDIR)/$(MODULATION)-$(GAROSREL)-$(GARCH)
-WORKDIR       = $(WORKROOTDIR)/build-$(MODULATION)-$(GAROSREL)-$(GARCH)
-PATCHDIR      = $(WORKDIR)/$(NAME)-$(VERSION)
-INSTALLISADIR = $(WORKROOTDIR)/install-$(MODULATION)-$(GAROSREL)-$(GARCH)
-PKGROOT       = $(abspath $(WORKROOTDIR)/pkgroot-$(GAROSREL)-$(GARCH))
-OBJECT_DIR    = $(WORKDIR)/objdir
-WORKSRC       = $(OBJECT_DIR)
+#COOKIEDIR     = $(COOKIEROOTDIR)/$(MODULATION)-$(GAROSREL)-$(GARCH)
+#WORKDIR       = $(WORKROOTDIR)/build-$(MODULATION)-$(GAROSREL)-$(GARCH)
+#PATCHDIR      = $(WORKDIR)/$(NAME)-$(VERSION)
+#INSTALLISADIR = $(WORKROOTDIR)/install-$(MODULATION)-$(GAROSREL)-$(GARCH)
+#PKGROOT       = $(abspath $(WORKROOTDIR)/pkgroot-$(GAROSREL)-$(GARCH))
+#OBJECT_DIR    = $(WORKDIR)/objdir
+#WORKSRC       = $(OBJECT_DIR)
 DIRPATHS      = 
 OPTFLAGS      =
 CONFIG_SHELL  = /opt/csw/bin/bash
 GARCOMPILER   = GCC3
-GCC3_CC       = /opt/csw/gcc3/bin/gcc -g -O2 -mcpu=v8 -pipe
-CFLAGS        = -I/opt/csw/include
-CPPFLAGS      = -I/opt/csw/include
-CXXFLAGS      = -I/opt/csw/include
-LDFLAGS       = -L/opt/csw/lib -R/opt/csw/lib
-BOOT_CFLAGS   = -I/opt/csw/include -mcpu=v8 -g -O2 -pipe
-BOOT_LDFLAGS  = -L/opt/csw/lib -R/opt/csw/lib
+#GCC3_CC       = /opt/csw/gcc3/bin/gcc -g -O2 -mcpu=v8 -pipe
+#CFLAGS        = -I/opt/csw/include
+#CPPFLAGS      = -I/opt/csw/include
+#CXXFLAGS      = -I/opt/csw/include
+#LDFLAGS       = -L/opt/csw/lib -R/opt/csw/lib
+#BOOT_CFLAGS   = -I/opt/csw/include -mcpu=v8 -g -O2 -pipe
+#BOOT_LDFLAGS  = -L/opt/csw/lib -R/opt/csw/lib
 
-ifeq ($(shell uname -p),i386)
-ifeq ($(shell uname -r),5.10)
-    GCC3_CC       = /opt/csw/gcc3/bin/gcc -g -O2 -pipe
-    BOOT_CFLAGS   = -I/opt/csw/include -g -O2 -pipe
-    BOOT_LDFLAGS  = -L/opt/csw/lib -R/opt/csw/lib
-else
-    GCC3_CC       = /opt/csw/gcc3/bin/gcc -march=i386 -g -O2 -pipe
-    BOOT_CFLAGS   = -I/opt/csw/include -m32 -march=i386 -g -O2 -pipe
-    BOOT_LDFLAGS  = -L/opt/csw/lib -R/opt/csw/lib
-endif
-endif
+#ifeq ($(shell uname -p),i386)
+#ifeq ($(shell uname -r),5.10)
+#    GCC3_CC       = /opt/csw/gcc3/bin/gcc -g -O2 -pipe
+#    BOOT_CFLAGS   = -I/opt/csw/include -g -O2 -pipe
+#    BOOT_LDFLAGS  = -L/opt/csw/lib -R/opt/csw/lib
+#else
+#    GCC3_CC       = /opt/csw/gcc3/bin/gcc -march=i386 -g -O2 -pipe
+#    BOOT_CFLAGS   = -I/opt/csw/include -m32 -march=i386 -g -O2 -pipe
+#    BOOT_LDFLAGS  = -L/opt/csw/lib -R/opt/csw/lib
+#endif
+#endif
 
-export CONFIG_SHELL CFLAGS CPPFLAGS CXXFLAGS
-export LDFLAGS BOOT_CFLAGS BOOT_LDFLAGS
+export CONFIG_SHELL
+#export CFLAGS CPPFLAGS CXXFLAGS
+#export LDFLAGS
+#export BOOT_CFLAGS BOOT_LDFLAGS
 
-FIXCONFIG_DIRS         += $(DESTDIR)$(BUILD_PREFIX)/gcc4/lib
-STRIP_DIRS             += $(DESTDIR)$(BUILD_PREFIX)/gcc4/bin
-CONFIGURE_SCRIPTS       = objdir
-TEST_SCRIPTS            = skip
-post-configure-modulated: fix-bootflags
+# FIXCONFIG_DIRS         += $(DESTDIR)$(BUILD_PREFIX)/gcc4/lib
+#STRIP_DIRS             += $(DESTDIR)$(BUILD_PREFIX)/gcc4/bin
+# CONFIGURE_SCRIPTS       = objdir
+
+SKIPTEST ?= 1
+
+# post-configure-modulated: fix-bootflags
 MERGE_SCRIPTS_isa-i386  = amd
 
 # <maciej> This should not be necessary any more thanks to checkpkg overrides.
--- csw/mgar/pkg/gcc4/trunk/files/pkg_scripts.mk
+++ csw/mgar/pkg/gcc4/trunk/files/pkg_scripts.mk
@@ -6,26 +6,26 @@ endif
 
 
 ## Create $(OBJECT_DIR) to build in
-post-extract-$(addprefix post-extract-,$(MODULATIONS)):
-	$(_DBG)(echo "==> Creating Object Dir for Building")
-	$(_DBG)(mkdir $(OBJECT_DIR))
-	$(_DBG)$(MAKECOOKIE)
+#post-extract-$(addprefix post-extract-,$(MODULATIONS)):
+#	$(_DBG)(echo "==> Creating Object Dir for Building")
+#	$(_DBG)(mkdir $(OBJECT_DIR))
+#	$(_DBG)$(MAKECOOKIE)
 
 ## instead of changing to $(WORKSRC) and running configure
 ## Run it from the $(OBJECT_DIR)
-configure-objdir:
-	$(_DBG)(echo "==> Running Configure from $(OBJECT_DIR)")
-	$(_DBG)(cd $(OBJECT_DIR) && $(CONFIGURE_ENV) \
-		../$(DISTNAME)/configure $(CONFIGURE_ARGS))
-	$(_DBG)$(MAKECOOKIE)
+#configure-objdir:
+#	$(_DBG)(echo "==> Running Configure from $(OBJECT_DIR)")
+#	$(_DBG)(cd $(OBJECT_DIR) && $(CONFIGURE_ENV) \
+#		../$(DISTNAME)/configure $(CONFIGURE_ARGS))
+#	$(_DBG)$(MAKECOOKIE)
 
 ## Set the CFLAGS so the correct architecture is used
-fix-bootflags:
-	$(_DBG)(perl -i -plne "s|^BOOT_CFLAGS.*|BOOT_CFLAGS= $(BOOT_CFLAGS)|" \
-		$(WORKSRC)/Makefile)
-	$(_DBG)(perl -i -plne "s|^BOOT_LDFLAGS.*|BOOT_LDFLAGS= $(BOOT_LDFLAGS)|" \
-		$(WORKSRC)/Makefile)
-	$(_DBG)$(MAKECOOKIE)
+#fix-bootflags:
+#	$(_DBG)(perl -i -plne "s|^BOOT_CFLAGS.*|BOOT_CFLAGS= $(BOOT_CFLAGS)|" \
+#		$(WORKSRC)/Makefile)
+#	$(_DBG)(perl -i -plne "s|^BOOT_LDFLAGS.*|BOOT_LDFLAGS= $(BOOT_LDFLAGS)|" \
+#		$(WORKSRC)/Makefile)
+#	$(_DBG)$(MAKECOOKIE)
 
 post-merge-modulated:
 	$(_DBG)( gmv $(PKGROOT)$(libdir)/gcc/*/*/adalib/*.so* \
@@ -37,8 +37,5 @@ post-merge-modulated:
 			else \
 				cd $(PKGROOT)$(libdir); gln -s amd64 64; \
 			fi )
-	$(_DBG)$(MAKECOOKIE)
-
-test-skip:
 	$(_DBG)$(MAKECOOKIE)
 
