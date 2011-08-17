3f4cd41c0f1f1c96ef740b91598cff170daa7dca
wahwah <wahwah@d3b55034-1cff-0310-a425-aefe953e1e90>
2011-08-17 20:03:39 +0000

    pkg/gcc4/branches/bootstrap-4.6: separating out scripts.mk

--- csw/mgar/pkg/gcc4/branches/bootstrap-4.6/files/package_def.mk
+++ csw/mgar/pkg/gcc4/branches/bootstrap-4.6/files/package_def.mk
@@ -1,4 +1,7 @@
 ###  Package Section  ###
+
+# Ada awaits being built.
+
 PACKAGES  = CSWgcc4ada
 CATALOGNAME_CSWgcc4ada        = gcc4ada
 SPKG_DESC_CSWgcc4ada        = GNU C ADA Compiler
@@ -14,12 +17,12 @@ RUNTIME_DEP_PKGS_CSWgcc4ada += CSWlibmpfr4
 RUNTIME_DEP_PKGS_CSWgcc4ada += CSWlibiconv2
 RUNTIME_DEP_PKGS_CSWgcc4ada += CSWlibgmp10
 
-PACKAGES += CSWgcc4adart
-CATALOGNAME_CSWgcc4adart      = gcc4adart
-SPKG_DESC_CSWgcc4adart      = GNU C ADA Compiler Run Time
-PKGFILES_CSWgcc4adart  = $(libdir)/.*libgnat.*\.so.*
-PKGFILES_CSWgcc4adart += $(libdir)/.*libgnarl.*\.so.*
-RUNTIME_DEP_PKGS_CSWgcc4adart       = CSWgcc4corert
+# PACKAGES += CSWgcc4adart
+# CATALOGNAME_CSWgcc4adart      = gcc4adart
+# SPKG_DESC_CSWgcc4adart      = GNU C ADA Compiler Run Time
+# PKGFILES_CSWgcc4adart  = $(libdir)/.*libgnat.*\.so.*
+# PKGFILES_CSWgcc4adart += $(libdir)/.*libgnarl.*\.so.*
+# RUNTIME_DEP_PKGS_CSWgcc4adart       = CSWgcc4corert
 
 # PACKAGES += CSWgcc4core
 # CATALOGNAME_CSWgcc4core       = gcc4core
@@ -69,77 +72,6 @@ RUNTIME_DEP_PKGS_CSWgcc4adart       = CSWgcc4corert
 # RUNTIME_DEP_PKGS_CSWgcc4objcrt      = CSWgcc4corert
 # PKGFILES_CSWgcc4objcrt = $(libdir)/.*libobjc.*\.so.*
  
-## Source URLs
-VENDOR_URL = http://gcc.gnu.org
-
-## Copyright File
-LICENSE = COPYING3
-
+# No idea what was that for.
 DISTFILES += CSWgcc4core.space
 
-define CSWgcc4core_postinstall
-#!/bin/sh
-
-Error()
-{
-	echo "=====> postinstall Error: $$1" >&2
-	exit 1
-}
-
-OS_REV="`/usr/bin/uname -r | sed -e 's/[^.]*//'`"
-case `/usr/bin/uname -p` in
-	"sparc") OS_TARGET="sparc-sun-solaris2.8" ;;
-	 "i386") OS_TARGET="i386-pc-solaris2$${OS_REV}" ;;
-esac
-
-TOOLS_DIR="$${PKG_INSTALL_ROOT}/opt/csw/gcc4/libexec/gcc"
-TOOLS_DIR="$${TOOLS_DIR}/$${OS_TARGET}/$(VERSION)/install-tools"
-MKHEADERS_CMD="$${PKG_INSTALL_ROOT}/opt/csw/gcc4/bin/mkheaders"
-INCLUDE_DIR="$${PKG_INSTALL_ROOT}/opt/csw/gcc4/lib/gcc"
-INCLUDE_DIR="$${INCLUDE_DIR}/$${OS_TARGET}/$(VERSION)/include"
-
-cat << _EOF_
-******************************************************************************
-* NOTICE: Fixing the system headers
-*
-* Do not forget: whenever your system headers change 
-* Run the $${MKHEADERS_CMD} script!
-******************************************************************************
-_EOF_
-
-if [ -f $${TOOLS_DIR}/mkheaders ]; then
-	cp $${TOOLS_DIR}/mkheaders $${MKHEADERS_CMD}
-	installf $${PKGINST} "$${MKHEADERS_CMD}"
-else
-	Error "$${TOOLS_DIR}/mkheaders Not Found"
-fi
-
-if [ -f $${MKHEADERS_CMD} ];then
-	chmod 0755 $${MKHEADERS_CMD} 2>/dev/null
-	chown root:bin $${MKHEADERS_CMD} 2>/dev/null
-	"$${MKHEADERS_CMD}" || Error "$${MKHEADERS_CMD} Failed."
-else
-	Error "Could not find $${MKHEADERS_CMD}"
-fi
-
-if [ -d $${INCLUDE_DIR} ]; then
-	chmod 0755 $${INCLUDE_DIR} || Error "Failed to chmod $${INCLUDE_DIR}"
-	chown -R root:bin $${INCLUDE_DIR} || 
-			Error "Failed to change ownership for $${INCLUDE_DIR}"
-	find $${INCLUDE_DIR} -print | installf $${PKGINST} -
-fi
-
-
-cat << _EOF_
-******************************************************************************
-* NOTICE: Successfully fixed system headers
-*
-* Do not forget: whenever your system headers change 
-* Run the $${MKHEADERS_CMD} script!
-******************************************************************************
-_EOF_
-	
-installf -f $${PKGINST}
-
-exit 0
-endef
