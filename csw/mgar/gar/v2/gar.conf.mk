a8df13ca5da336f27c3dfb5150bea6b5fe689a1d
dmichelsen <dmichelsen@d3b55034-1cff-0310-a425-aefe953e1e90>
2012-02-01 13:44:27 +0000

    mGAR v2: Use only 64 bit ISA in BUILD64_ONLY

--- csw/mgar/gar/v2/gar.conf.mk
+++ csw/mgar/gar/v2/gar.conf.mk
@@ -503,7 +503,7 @@ $(if $(BUILD64_ONLY),$(eval ISAEXEC=1) $(eval BUILD64=1))
 # Just keeps the first appearance of each word
 remove-duplicates = $(if $1,$(firstword $1) $(call remove-duplicates,$(filter-out $(firstword $1),$1)))
 
-REQUESTED_ISAS ?= $(strip $(call remove-duplicates,$(foreach A,$(GARCHLIST),$(ISA_DEFAULT_$A) $(if $(or $(BUILD64),$(BUILD64_LIBS_ONLY)),$(ISA_DEFAULT64_$A)) $(EXTRA_BUILD_ISAS_$A)) $(EXTRA_BUILD_ISAS)))
+REQUESTED_ISAS ?= $(strip $(call remove-duplicates,$(foreach A,$(GARCHLIST),$(if $(BUILD64_ONLY),,$(ISA_DEFAULT_$A)) $(if $(or $(BUILD64),$(BUILD64_LIBS_ONLY)),$(ISA_DEFAULT64_$A)) $(EXTRA_BUILD_ISAS_$A)) $(EXTRA_BUILD_ISAS)))
 NEEDED_ISAS ?= $(strip $(filter     $(ISALIST_$(KERNELISA)),$(filter $(ISALIST_$(ISA_DEFAULT64_$(GARCH))),$(REQUESTED_ISAS))) \
                        $(filter-out $(ISALIST_$(KERNELISA)),$(filter $(ISALIST_$(ISA_DEFAULT64_$(GARCH))),$(REQUESTED_ISAS))))
 BUILD_ISAS ?= $(filter $(ISALIST_$(KERNELISA)),$(NEEDED_ISAS))
