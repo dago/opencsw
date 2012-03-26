6286ffda350a7a23a4c60396d1335355a8aa16ec
d_pocock <d_pocock@d3b55034-1cff-0310-a425-aefe953e1e90>
2012-03-26 19:15:06 +0000

    ganglia: re-define other relative paths after changing conf_dir base

--- csw/mgar/pkg/ganglia/trunk/files/conf.php
+++ csw/mgar/pkg/ganglia/trunk/files/conf.php
@@ -7,5 +7,8 @@ $conf['dwoo_cache_dir'] = "${conf['gmetad_root']}/dwoo_cache";
 $conf['views_dir'] = $conf['gmetad_root'] . '/conf';
 $conf['conf_dir'] = $conf['gmetad_root'] . '/conf';
 
+$conf['overlay_events_file'] = $conf['conf_dir'] . "/events.json";
+$conf['overlay_events_color_map_file'] = $conf['conf_dir'] . "/event_color.json";
+
 $conf['rrdtool'] = "/opt/csw/bin/rrdtool";
 ?>
