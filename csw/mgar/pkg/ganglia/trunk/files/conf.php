14b66e69d928877de083b90b90f7fdf760a7cfe2
d_pocock <d_pocock@d3b55034-1cff-0310-a425-aefe953e1e90>
2012-03-26 11:21:15 +0000

    ganglia: add dwoo_cache directory

--- csw/mgar/pkg/ganglia/trunk/files/conf.php
+++ csw/mgar/pkg/ganglia/trunk/files/conf.php
@@ -3,6 +3,7 @@
 $conf['gmetad_root'] = "/var/opt/csw/ganglia";
 $conf['rrds'] = "${conf['gmetad_root']}/rrds";
 $conf['dwoo_compiled_dir'] = "${conf['gmetad_root']}/dwoo";
+$conf['dwoo_cache_dir'] = "${conf['gmetad_root']}/dwoo_cache";
 $conf['views_dir'] = $conf['gmetad_root'] . '/conf';
 $conf['conf_dir'] = $conf['gmetad_root'] . '/conf';
 
