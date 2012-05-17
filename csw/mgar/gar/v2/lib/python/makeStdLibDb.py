9876f7550bd7b43df185d37e6e8ac251103162b4
wahwah <wahwah@d3b55034-1cff-0310-a425-aefe953e1e90>
2012-05-17 16:19:31 +0000

    makeStdLibDb: Normalize whitespace
    Tabs changed to spaces.
    
    The curse of Python.

--- csw/mgar/gar/v2/lib/python/makeStdLibDb.py
+++ csw/mgar/gar/v2/lib/python/makeStdLibDb.py
@@ -16,29 +16,29 @@ def buildStdlibList():
   cwd = getcwd()
   chdir('/usr/lib')
   for lib in listdir('.'):
-	if re.match('lib[a-zA-Z0-9_-]*.so.[0-9]+$',lib):
-		liblst.append(lib)
+  if re.match('lib[a-zA-Z0-9_-]*.so.[0-9]+$',lib):
+    liblst.append(lib)
   chdir('/usr/dt/lib')
   for lib in listdir('.'):
-	if re.match('lib[a-zA-Z0-9_-]*.so.[0-9]+$',lib):
-	    if not lib in liblst:
-	        liblst.append(lib)
+  if re.match('lib[a-zA-Z0-9_-]*.so.[0-9]+$',lib):
+      if not lib in liblst:
+          liblst.append(lib)
   chdir('/usr/openwin/lib')
   for lib in listdir('.'):
-	if re.match('lib[a-zA-Z0-9_-]*.so.[0-9]+$',lib):
-	    if not lib in liblst:
-	        liblst.append(lib)
+  if re.match('lib[a-zA-Z0-9_-]*.so.[0-9]+$',lib):
+      if not lib in liblst:
+          liblst.append(lib)
   chdir('/usr/X11/lib')
   for lib in listdir('.'):
-	if re.match('lib[a-zA-Z0-9_-]*.so.[0-9]+$',lib):
-	    if not lib in liblst:
-	        liblst.append(lib)
+  if re.match('lib[a-zA-Z0-9_-]*.so.[0-9]+$',lib):
+      if not lib in liblst:
+          liblst.append(lib)
   chdir('/usr/ucblib')
   for lib in listdir('.'):
-	if re.match('lib[a-zA-Z0-9_-]*.so.[0-9]+$',lib):
-	    if not lib in liblst:
-	        liblst.append(lib)
+  if re.match('lib[a-zA-Z0-9_-]*.so.[0-9]+$',lib):
+      if not lib in liblst:
+          liblst.append(lib)
   chdir(cwd)
   with open(fnLiblst,'w') as fd:
-	fd.write(cjson.encode(liblst))
+  fd.write(cjson.encode(liblst))
   fd.close()
