8077fad7c705368cf504e69bc388f090ef12cfc6
wahwah <wahwah@d3b55034-1cff-0310-a425-aefe953e1e90>
2012-12-27 17:48:16 +0000

    catalog-gc: Bugfix and update usage info

--- csw/mgar/gar/v2/lib/python/catalog_gc.py
+++ csw/mgar/gar/v2/lib/python/catalog_gc.py
@@ -1,15 +1,24 @@
 #!/opt/csw/bin/python2.6
 
-"""Garbage-collecting for a catalog.
+"""Garbage-collecting for the catalog tree.
 
 The allpkgs directory may contain unused files.  They should be deleted.
 """
 
-import optparse
 import logging
+import optparse
 import os.path
+import pipes
 import re
 import common_constants
+
+USAGE = """%prog --catalog-tree /home/mirror/opencsw-official > gc_01.sh
+less gc_01.sh
+
+# Looks good?
+
+bash gc_01.sh
+"""
 
 class Error(Exception):
   """Base error."""
@@ -20,7 +29,7 @@ class UsageError(Error):
 
 class CatalogGarbageCollector(object):
 
-  ADDITIONAL_CATALOGS = ("legacy")
+  ADDITIONAL_CATALOGS = ("legacy",)
 
   def __init__(self, d):
     logging.debug("CatalogGarbageCollector(%s)", repr(d))
@@ -53,21 +62,21 @@ class CatalogGarbageCollector(object):
               l = catalogs_by_files.setdefault(p, [])
               l.append((catrel, arch, osrel_short))
     for p in allpkgs.difference(files_in_catalogs):
-      logging.debug("File %s is not used by any catalogs.", p)
-      print "rm %s/%s" % (allpkgs_path, p)
+      print "rm", pipes.quote(os.path.join(allpkgs_path, p))
 
 
 def main():
   parser = optparse.OptionParser()
-  parser.add_option("-c", "--catalog",
-      dest="catalog",
-      help="Catalog path")
+  parser.add_option("--catalog-tree",
+      dest="catalog_tree",
+      help=("Path to the catalog tree, that is the directory "
+            "containing subdirectories unstable, testing, etc."))
   options, args = parser.parse_args()
   logging.basicConfig(level=logging.DEBUG)
-  if not options.catalog:
+  if not options.catalog_tree:
     parser.print_usage()
-    raise UsageError("Missing catalog option, see --help.")
-  gcg = CatalogGarbageCollector(options.catalog)
+    raise UsageError("Missing the catalog tree option, see --help.")
+  gcg = CatalogGarbageCollector(options.catalog_tree)
   gcg.GarbageCollect()
 
 
