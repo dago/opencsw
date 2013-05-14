47c48498484f06614c78eb9c0a90351e499204e1
wahwah <wahwah@d3b55034-1cff-0310-a425-aefe953e1e90>
2013-05-14 16:48:11 +0000

    pkgdb: Small bugfix for representations.py

--- csw/mgar/gar/v2/lib/python/representations.py
+++ csw/mgar/gar/v2/lib/python/representations.py
@@ -2,5 +2,5 @@ import collections
 
 # Full catalog entry, enough to write a line of a catalog file.
 CatalogEntry = collections.namedtuple(
-    'CatalogEntry', 'catalogname version pkgname basename'
+    'CatalogEntry', 'catalogname version pkgname basename '
                     'md5_sum size deps category i_deps')
