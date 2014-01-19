2876f1d9a5e464418134e919ffa789ef5a87a1d7
chninkel <chninkel@d3b55034-1cff-0310-a425-aefe953e1e90>
2014-01-19 17:48:29 +0000

    openssl1/trunk: Updated t4 engine patch from Oracle userland sources

--- csw/mgar/pkg/openssl1/trunk/files/update-t4-patch.sh
+++ csw/mgar/pkg/openssl1/trunk/files/update-t4-patch.sh
@@ -4,10 +4,8 @@
 #                   hg.openindiana.org repository 
 #           
 
-T4_ENGINE_FILES="eng_t4_aes_asm.h eng_t4_bignum.h eng_t4_des_asm.h eng_t4_err.h eng_t4_sha2_asm.h \
-                 eng_t4.c eng_t4_des.c eng_t4_err.c eng_t4_md5.c eng_t4_montmul.c eng_t4_sha1.c eng_t4_sha256.c eng_t4_sha512.c \
-                 t4_aes.S t4_des.S t4_md5.S t4_sha1.S t4_sha2.S"
-MERCURIAL_URL="https://hg.openindiana.org/upstream/oracle/userland-gate/raw-file/tip/components/openssl/openssl-1.0.1/"
+T4_ENGINE_FILES="sparc_arch.h md5-sparcv9.pl aest4-sparcv9.pl dest4-sparcv9.pl sparcv9_modes.pl vis3-mont.pl sparcv9-gf2m.pl sparct4-mont.pl"
+SOURCE_URL="http://buildfarm.opencsw.org/source/raw/solaris-userland/components/openssl/openssl-1.0.1/"
 WGET_OPTIONS="--quiet"
 
 
@@ -17,32 +15,36 @@ if [[ -z "$1" ]]; then
 fi
 
 VERSION="$1"
-PATCH_FILE="openssl-${VERSION}-t4-engine.sparc-patch"
+PATCH_FILE="openssl-${VERSION}-t4-engine.sparc.5.11.patch"
 PATCH_DATE=$(date +"%Y-%m-%d %H:%M:%S.%N %z")
 
-echo "Updating t4 engine patch from ${MERCURIAL_URL}..."
+echo "Updating t4 engine patch from ${SOURCE_URL}..."
 (
 	# ar in in /usr/ccs/bin under Solaris 9 and 10 so we change the path
-	wget $WGET_OPTIONS --output-document=- ${MERCURIAL_URL}/patches/openssl-${VERSION}-t4-engine.sparc-patch | \
+	wget $WGET_OPTIONS --output-document=- ${SOURCE_URL}/patches/openssl-t4-inline.sparc-patch | \
 		sed -e 's/\/usr\/bin\/ar/\/usr\/ccs\/bin\/ar/g'
 
 	# in the repository, the new files are not part of the patch, but we merge them
 	# in a single patch
 	for FILE in $T4_ENGINE_FILES; do \
-		if [[ "${FILE##*.}" != "S" ]]; then
-			DIR="engine"
+		if [[ "${FILE##*.}" = "h" ]]; then
+			DIR="crypto"
+		elif echo "$FILE" | grep -- '-sparcv9.pl' >/dev/null; then
+			DIR="crypto/${FILE:0:3}/asm"
+		elif [[ "$FILE" = "sparcv9_modes.pl" ]]; then
+			DIR="crypto/perlasm"
 		else
-			DIR="${FILE:3:3}/asm"
+			DIR="crypto/bn/asm"
 		fi
 
-		wget $WGET_OPTIONS --output-document="${PATCH_FILE}.tmp" "${MERCURIAL_URL}/engines/t4/$FILE"
+		wget $WGET_OPTIONS --output-document="${PATCH_FILE}.tmp" "${SOURCE_URL}/inline-t4/$FILE"
 		NB_LINES=$(wc -l "${PATCH_FILE}.tmp" | awk '{ print $1 }')
 
-		echo "Index: crypto/$DIR/$FILE"
+		echo "Index: $DIR/$FILE"
 		echo "==================================================================="
 		echo "diff -uNr openssl-${VERSION}/$DIR/$FILE openssl-${VERSION}/$DIR/$FILE"
-		echo "--- openssl-${VERSION}/crypto/$DIR/$FILE 1970-01-01 01:00:00.000000000 +0100"
-		echo "+++ openssl-${VERSION}/crypto/$DIR/$FILE ${PATCH_DATE}"
+		echo "--- openssl-${VERSION}/$DIR/$FILE 1970-01-01 01:00:00.000000000 +0100"
+		echo "+++ openssl-${VERSION}/$DIR/$FILE ${PATCH_DATE}"
 		echo "@@ -0,0 +1,${NB_LINES} @@"
 		sed -e 's/^/+/' "${PATCH_FILE}.tmp"
 	done
